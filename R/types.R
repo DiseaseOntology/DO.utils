909a58f1d8d6ebfd4a01c73302451372c57eb88d
J. Allen Baron <jabaron.phd@gmail.com>
2021-07-07 17:29:48 -0400

    Create character and numeric value predicates

---
+++ R/types.R
@@ -0,0 +1,51 @@
+#' Character value predicates
+#'
+#' These value predicates are designed to identify common values that appear
+#' in character vectors.
+#'
+#' * `is_blank()` identifies "" or whitespace of any length
+#'
+#' * `is_missing()` identifies NA's and blanks
+#'
+#' @param x vector to be tested
+#' @name char_val_predicates
+NULL
+
+#' @export
+#' @rdname char_val_predicates
+is_blank <- function(x) {
+    assert_character(x)
+    stringr::str_trim(x) == ""
+}
+
+#' @export
+#' @rdname char_val_predicates
+is_missing <- function(x) {
+    assert_character(x)
+    is.na(x) | is_blank(x)
+}
+
+
+#' Number value predicates
+#'
+#' These value predicates are designed to identify common values that appear
+#' in numeric vectors.
+#'
+#'
+#' @inheritParams char_val_predicates
+#' @name num_val_predicates
+NULL
+
+#' @export
+#' @rdname num_val_predicates
+is_positive <- function(x) {
+    assert_numeric(x)
+    x > 0 & is.finite(x)
+}
+
+#' @export
+#' @rdname num_val_predicates
+is_negative <- function(x) {
+    assert_numeric(x)
+    x < 0 & is.finite(x)
+}
