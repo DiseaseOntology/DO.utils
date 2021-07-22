19a13788e8f49eea4a7d27e126654f5d7e801b39
J. Allen Baron <jabaron.phd@gmail.com>
2021-07-22 17:05:17 -0400

    Add Bioconductor stat page base urls

--- R/bioc_impact.R
+++ R/bioc_impact.R
@@ -3,8 +3,8 @@ DO_dep_pkg <- list(software = "DOSE", annotation = "DO.db")
 
 bioc_pkg_types <- c("software", "annotation", "experiment", "workflow")
 
-# list bioconductor statistics web resources - main site, statistics dump file,
-#   download score dump file
+# list bioconductor statistics web resources - main site, base urls,
+#   statistics dump file, download score dump file
 #
 #   * download score = average downloads over 12 month period
 bioc_stat_sites <- purrr::set_names(
@@ -17,9 +17,19 @@ bioc_stat_sites <- purrr::set_names(
         bioc_pkg_types
     )
 
+bioc_stat_baseurl <- purrr::set_names(
+    c(
+        "http://bioconductor.org/packages/stats/bioc/",
+        "http://bioconductor.org/packages/stats/data-annotation/",
+        "http://bioconductor.org/packages/stats/data-experiment/",
+        "http://bioconductor.org/packages/stats/workflows/"
+    ),
+    bioc_pkg_types
+)
+
 bioc_stat_dumps <- purrr::set_names(
         stringr::str_replace_all(
-            bioc_stat_sites,
+            bioc_stat_baseurl,
             c("stats/$" = "stats/bioc/bioc_pkg_stats.tab",
               "([a-z]+)\\.html" = "\\1/\\1_pkg_stats.tab")
         ),
