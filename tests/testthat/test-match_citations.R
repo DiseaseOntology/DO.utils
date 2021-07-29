a63e7aa6705816411a8cfe3b6043cbe42a9600c0
J. Allen Baron <jabaron.phd@gmail.com>
2021-07-29 16:58:39 -0400

    Add tests for find_pub_id_cols() [pass]

--- tests/testthat/test-match_citations.R
+++ tests/testthat/test-match_citations.R
@@ -1,12 +1,14 @@
 
-# type_pub_id() tests -----------------------------------------------------
+# Data --------------------------------------------------------------------
 
-# data
+# vctr - good
 pmid <- c("33244381", "32308857", "30815196", "30815087", "30815154")
 pmcid <- c("PMC6371329", "PMC6371303", "PMC6371266", "PMC7153069",
-                "PMC7682973")
+           "PMC7682973")
 doi <- c("10.1093/nar/gki050", "10.1093/nar/gkm829", "10.1038/nbt1346",
-              "10.1093/nar/gkm1021", "10.1186/1471-2105-8-S9-S1")
+         "10.1093/nar/gkm1021", "10.1186/1471-2105-8-S9-S1")
+
+# vctr - bad
 mix <- c(pmid[1], pmcid[1], doi[1])
 none <- c(
     "A", # random letter
@@ -15,9 +17,35 @@ none <- c(
     # missing 'P' of 'PMC',
     "MC7682973"
 )
-pmid_na <- c("33244381", NA_character_, "30815196", NA_character_, "30815154")
 
-# tests
+# NA
+pmid_na <- c("33244381", "32308857", NA, NA, NA)
+pmcid_na <- c("PMC6371329", "PMC6371303", "PMC6371266", NA, NA)
+doi_na <- c("10.1093/nar/gki050", "10.1093/nar/gkm829", "10.1038/nbt1346",
+         "10.1093/nar/gkm1021", NA)
+
+# df - good
+df_pmid <- data.frame(n = 1:5, pmid = pmid)
+df_all <- dplyr::mutate(df_pmid, pmcid = pmcid, doi = doi)
+
+# df - bad
+df_wrong_name <- dplyr::rename(df_pmid, bad_name = pmid)
+
+
+# find_pub_id_cols() tests ------------------------------------------------
+
+test_that("it works", {
+    expect_equal(find_pub_id_cols(df_pmid), "pmid")
+    expect_equal(find_pub_id_cols(df_all), c("pmid", "pmcid", "doi"))
+})
+
+test_that("missing col fails", {
+    expect_error(find_pub_id_cols(df_wrong_name), NULL)
+})
+
+
+# type_pub_id() tests -----------------------------------------------------
+
 test_that("types identifiable", {
     expect_equal(type_pub_id(pmid), "pmid")
     expect_equal(type_pub_id(pmcid), "pmcid")
