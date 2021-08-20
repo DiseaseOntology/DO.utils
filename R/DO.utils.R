9a26a60d54a49a529d6b3371a9ace160d516749c
J. Allen Baron <jabaron.phd@gmail.com>
2021-08-20 14:18:26 -0400

    Add package help (basic)

---
+++ R/DO.utils.R
@@ -0,0 +1,10 @@
+#' Human Disease Ontology (DO) support package
+#'
+#' The DO.utils packages provides functions that assist in three main areas:
+#' 1. Managing DO tools/builds
+#' 2. Assessing DO's impact on science (bibliometrics/scientometrics)
+#' 3. Making R programming simpler/faster
+#'
+#' @docType package
+#' @name DO.utils
+NULL
