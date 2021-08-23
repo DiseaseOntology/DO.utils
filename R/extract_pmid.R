fa4da18aafe95d394c86c79b03ad4184723f38ac
J. Allen Baron <jabaron.phd@gmail.com>
2021-08-23 17:45:14 -0400

    Make extract_pmid.pmc_search() warning more informative

--- R/extract_pmid.R
+++ R/extract_pmid.R
@@ -24,8 +24,17 @@ extract_pmid.pmc_search <- function(x) {
         stop("No PMIDs available. Was 'pmid' set to TRUE in search_pmc()?")
     }
 
-    if (any(is.na(pmids))) {
-        warning("Not all PMIDs were available. Consider using PMCIDs instead")
+    pmid_missing <- is.na(pmids)
+    if (any(pmid_missing)) {
+        n_missing <- sum(pmid_missing)
+        n_id <- length(pmids)
+        pct_missing <- round(n_missing / n_id, 2)
+
+        warning(
+            n_missing, " of ", n_id, " (", pct_missing, "%)",
+            " PMIDs are missing. Consider extracting PMCIDs.",
+            call. = FALSE
+        )
     }
 
     pmids
