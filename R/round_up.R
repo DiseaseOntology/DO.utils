b6f42baf9ae4d02b330820971a2b38baa591d3e3
J. Allen Baron <jabaron.phd@gmail.com>
2021-08-24 00:04:22 -0400

    Copy round_up() from JABmisc

---
+++ R/round_up.R
@@ -0,0 +1,20 @@
+#' Round a number up
+#'
+#' @details
+#' Rounding to a negative number of digits means rounding to a power of ten, so
+#' for example round_up(x, digits = -2) rounds to the nearest hundred.
+#'
+#' @param x a numeric vector.
+#' @param digits integer indicating the number of decimal places. Negative
+#' values are allowed (see 'Details').
+#'
+#' @export
+round_up <- function(x, digits = 0) {
+
+    if (digits == 0) {
+        return(ceiling(x))
+    }
+
+    place <- 10^(-digits)
+    x + place - x %% place
+}
