72ec6f49cd6c6169f731869359d5d0da0a7fd73c
J. Allen Baron <jabaron.phd@gmail.com>
2021-09-04 18:11:26 -0400

    Prep alliance_version() for S3 methods (new file)

--- R/alliance.R
+++ R/alliance.R
@@ -255,56 +255,6 @@ count_alliance_records <- function(alliance_tbl,
 
 # Alliance helpers --------------------------------------------------------
 
-#' Get Alliance Version
-#'
-#' Gets version of Alliance of Genome Resources directly from .tsv file header.
-#'
-#' @param alliance_tsv Alliance .tsv data file
-#' @param as_string if FALSE (default), returns Alliance version and file
-#' datetime as list; if TRUE, returns Alliance version and file datetime as
-#' string
-#'
-#' @noRd
-alliance_version <- function(alliance_tsv, as_string = FALSE) {
-
-    # validate
-    assertthat::assert_that(rlang::is_scalar_logical(as_string))
-
-    header <- readLines(alliance_tsv, n = 30)
-    version_date <- grep(
-        "^#.*(version|date).*:",
-        header,
-        ignore.case = TRUE,
-        value = TRUE
-    ) %>%
-        stringr::str_remove("#") %>%
-        stringr::str_squish() %>%
-        stringr::str_split(": ")
-
-    if (as_string) {
-        vd_string <- version_date %>%
-            purrr::map(2) %>%
-            unlist() %>%
-            vctr_to_string(delim = "_") %>%
-            stringr::str_replace_all(
-                c("[-:]" = "", " " = "_")
-            )
-
-        return(vd_string)
-    }
-
-    vd_list <- purrr::set_names(
-        purrr::map(version_date, 2),
-        nm = stringr::str_replace_all(
-            purrr::map(version_date, 1),
-            c(" " = "_", "[()]" = "")
-        )
-    )
-
-    vd_list
-}
-
-
 #' Identifies Alliance MOD
 #'
 #' Replace Alliance MOD codes with recognizable abbreviation or name used by
---
+++ R/alliance_version.R
@@ -0,0 +1,49 @@
+#' Get Alliance Version
+#'
+#' Gets version of Alliance of Genome Resources directly from .tsv file header.
+#'
+#' @param alliance_obj an Alliance .tsv data file OR an Alliance (count) tibble
+#' @param as_string if FALSE (default), returns Alliance version and file
+#' datetime as list; if TRUE, returns Alliance version and file datetime as
+#' string
+#'
+#' @noRd
+alliance_version <- function(alliance_obj, as_string = FALSE) {
+
+    # validate
+    assertthat::assert_that(rlang::is_scalar_logical(as_string))
+
+    header <- readLines(alliance_tsv, n = 30)
+    version_date <- grep(
+        "^#.*(version|date).*:",
+        header,
+        ignore.case = TRUE,
+        value = TRUE
+    ) %>%
+        stringr::str_remove("#") %>%
+        stringr::str_squish() %>%
+        stringr::str_split(": ")
+
+    if (as_string) {
+        vd_string <- version_date %>%
+            purrr::map(2) %>%
+            unlist() %>%
+            vctr_to_string(delim = "_") %>%
+            stringr::str_replace_all(
+                c("[-:]" = "", " " = "_")
+            )
+
+        return(vd_string)
+    }
+
+    vd_list <- purrr::set_names(
+        purrr::map(version_date, 2),
+        nm = stringr::str_replace_all(
+            purrr::map(version_date, 1),
+            c(" " = "_", "[()]" = "")
+        )
+    )
+
+    vd_list
+}
+
