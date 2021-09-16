2e2459b4e5535783a3feead9be7448e15ffaf079
J. Allen Baron <jabaron.phd@gmail.com>
2021-09-16 12:45:59 -0400

    Add DO publication dataset (includes IDs, citation)

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Human Disease Ontology (DO) Publication Info
+#'
+#' A dataset of identifiers for official DO publications, along with their
+#' title and NLM-formatted citations.
+#'
+#' @format A data frame with 8 rows and 7 variables:
+#' \describe{
+#'   \item{internal_id}{short-version identifier used by
+#' the DO team}
+#'   \item{pmid}{PubMed ID}
+#'   \item{pmcid}{PubMed Central ID}
+#'   \item{doi}{DOI}
+#'   \item{semantic_scholar_id}{Semantic Scholar Corpus ID}
+#'   \item{title}{Publication Title}
+#'   \item{citation_nlm}{full NLM-formatted citation}
+#' }
+#'
+#' @source "Compiled by J. Allen Baron; last updated 2021-09-16"
+"DO_pubs"
