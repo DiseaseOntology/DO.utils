eba80931357a2de3b897d1cedd5bb3dfc472ec08
J. Allen Baron <jabaron.phd@gmail.com>
2021-09-18 13:32:30 -0400

    Switch pubmed_summary retmode default to avoid limit
    PROBLEM: retmode = "json" has 500 record limit, which is too low
    for even regular use.
    
    FIX: Set retmode = "xml" as default; both result in similar,
    though not identical, output from rentrez::entrez_summary()
    which can be parsed correctly by DO.utils::tidy.

--- R/pubmed.R
+++ R/pubmed.R
@@ -5,12 +5,15 @@
 #' IDs as input (using [rentrez::entrez_post]).
 #'
 #' @inheritParams rentrez::entrez_summary
-#' @param version,retmode,always_return_list preferred defaults are set but
+#' @param version,always_return_list preferred defaults are set but
 #' included here for flexibility; see [rentrez::entrez_summary()] for details
+#' @param retmode "xml" (default) or "json"; "xml" is preferred because of a
+#' higher limit. This default is opposite the [rentrez::entrez_summary()]
+#' default.
 #'
 #' @export
 pubmed_summary <- function(id = NULL, web_history = NULL, config = NULL,
-                           version = "2.0", retmode = "json",
+                           version = "2.0", retmode = "xml",
                            always_return_list = TRUE, ...) {
 
     if (is.null(web_history) & length(id) > 200) {
