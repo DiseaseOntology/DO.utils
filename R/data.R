f47f7b0e2450796c6f5426d7e98af370c7afddd5
J. Allen Baron <jabaron.phd@gmail.com>
2021-09-27 17:35:32 -0400

    Add Scopus EID to DO publication info

--- R/data.R
+++ R/data.R
@@ -3,17 +3,17 @@
 #' A dataset of identifiers for official DO publications, along with their
 #' title and NLM-formatted citations.
 #'
-#' @format A data frame with 8 rows and 7 variables:
+#' @format A data frame with 8 rows and 8 variables:
 #' \describe{
-#'   \item{internal_id}{short-version identifier used by
-#' the DO team}
+#'   \item{internal_id}{short-version identifier used by the DO team}
 #'   \item{pmid}{PubMed ID}
 #'   \item{pmcid}{PubMed Central ID}
 #'   \item{doi}{DOI}
+#'   \item{scopus_eid}{Scopus Electronic Identification}
 #'   \item{semantic_scholar_id}{Semantic Scholar Corpus ID}
 #'   \item{title}{Publication Title}
 #'   \item{citation_nlm}{full NLM-formatted citation}
 #' }
 #'
-#' @source "Compiled by J. Allen Baron; last updated 2021-09-16"
+#' @source Compiled by J. Allen Baron; last updated 2021-09-27
 "DO_pubs"
