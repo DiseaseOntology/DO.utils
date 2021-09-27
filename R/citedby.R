2a2cb6c5ca5fbb5b882c02b2b5f840c14c02a54b
J. Allen Baron <jabaron.phd@gmail.com>
2021-09-27 11:09:56 -0400

    Improve documentation

--- R/citedby.R
+++ R/citedby.R
@@ -1,8 +1,8 @@
-#' Gets Cited By List from PubMed
+#' Get Cited By List from PubMed
 #'
-#' Lists the publications citing those specified by `web_history` or `id`,
-#' potentially split `by_id`. `pubmed_citedby` can handle cases where the
-#' number of input IDs is >200.
+#' List PubMed IDs of publications citing those specified by `web_history` or
+#' `id`, potentially split `by_id`. `citedby_pubmed` can handle cases where the
+#' number of input IDs is >200 automatically (unlike [rentrez::entrez_link]).
 #'
 #' @inheritParams rentrez::entrez_link
 #' @export
@@ -22,7 +22,8 @@ citedby_pubmed <- function(id = NULL, web_history = NULL, by_id = FALSE,
         dbfrom = "pubmed",
         db = "pubmed",
         cmd = "neighbor",
-        linkname = "pubmed_pubmed_citedin"
+        linkname = "pubmed_pubmed_citedin",
+        ...
     )
 
     cited_by
