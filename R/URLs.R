a0953f7f4ecfd0ff0a4996c323cc8c256fd6e40b
J. Allen Baron <jabaron.phd@gmail.com>
2021-10-06 10:45:04 -0400

    Add internal fxn to access relevant URLs by name

---
+++ R/URLs.R
@@ -0,0 +1,22 @@
+#' Get URL (internal)
+#'
+#' Get a URL used within this package. Available URLs: "doi", "pubmed", "pmc",
+#' "pmc_article" (append as prefix to article pmcid for direct navigation),
+#' "alliance_disease_tsv"
+#'
+#' @param .name internal name of desired URL
+get_url <- function(.name) {
+    .name <- match.arg(
+        .name,
+        c("doi", "pubmed", "pmc", "pmc_article", "alliance_disease_tsv")
+    )
+
+    switch(
+        .name,
+        doi = "https://www.doi.org/",
+        pubmed = "https://pubmed.ncbi.nlm.nih.gov/",
+        pmc = "https://www.ncbi.nlm.nih.gov/pmc/",
+        pmc_article = "https://www.ncbi.nlm.nih.gov/pmc/articles/",
+        alliance_disease_tsv = "https://fms.alliancegenome.org/download/DISEASE-ALLIANCE_COMBINED.tsv.gz"
+    )
+}
