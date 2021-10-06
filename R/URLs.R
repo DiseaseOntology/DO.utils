82bba8f9e0ed120725fec41e5292087896b1b19e
J. Allen Baron <jabaron.phd@gmail.com>
2021-10-06 10:47:31 -0400

    Create function to append variables to URLs
    URL is treated as an actual URL (with no tests for validity) if
    anything other than an internal URL name is given.

--- R/URLs.R
+++ R/URLs.R
@@ -20,3 +20,29 @@ get_url <- function(.name) {
         alliance_disease_tsv = "https://fms.alliancegenome.org/download/DISEASE-ALLIANCE_COMBINED.tsv.gz"
     )
 }
+
+#' Append to URL
+#'
+#' Append a value to a URL.
+#'
+#' @section Note:
+#' No URL validation is performed.
+#'
+#' @param x value to append
+#' @param url a URL or the internal name of a URL used in this package (see
+#' [get_url] for possible names)
+#'
+#' @export
+append_to_url <- function(x, url) {
+
+    url <- tryCatch(get_url(url), error = function(e) url)
+
+    # add '/' if no terminal '/' in URL
+    if (stringr::str_detect(url, "/$")) {
+        new_url <- paste0(url, x)
+    } else {
+        new_url <- paste0(url, "/", x)
+    }
+
+    new_url
+}
