5e31fbd6ed2dfe65daa29d1f48bf8fb3b9f2ab1e
J. Allen Baron <jabaron.phd@gmail.com>
2021-10-14 11:47:38 -0400

    Create functions to convert lists to/from json strings
    PURPOSE:
    To convert nested list columns of data.frames so that they can
    be stored in a tabular files without data loss.
    
    PROBLEM:
    Scopus & PubMed API results end up with some nested list
    columns. These can be saved to file as R objects but that's
    not ideal for version control. When saved in tabular format
    (e.g. .csv) the data in these columns is lost.
    
    FIX:
    - confine_list(): converts lists to character vector of same
    - release_list(): converts character vector of json strings back
    into original list

---
+++ R/confine_list.R
@@ -0,0 +1,39 @@
+#' Convert (Nested) List to/from Character Vector
+#'
+#' These functions convert a (nested) list to or from a character vector of the
+#' same length, preserving or restoring the list structure and values with JSON
+#' strings. They are primarily designed to preserve the structure of simple
+#' nested lists (no internal data.frames, matrices, or arrays), as would result
+#' from an API call converted to a list without any conversion
+#' (e.g. flattening).
+#'
+#' @section NOTE:
+#' These could potentially be made to handle more diverse lists by including
+#' options that control how data.frames & matrices are managed, ensuring
+#' to/from procedures are linked and occur in predictable ways (see
+#' arguments for [jsonlite::to/fromJSON](jsonlite::fromJSON) for details).
+#'
+#' @param .list a (nested) list
+#' @param .json_chr a character vector containing JSON representation of list
+#' elements below the top level
+#'
+#' @export
+confine_list <- function(.list) {
+    purrr::map_chr(
+        .list,
+        jsonlite::toJSON,
+        null = "null"
+    )
+}
+
+#' @rdname confine_list
+#' @export
+release_list <- function(.json_chr) {
+    purrr::map(
+        .json_chr,
+        jsonlite::fromJSON,
+        simplifyVector = TRUE,
+        simplifyDataFrame = FALSE,
+        simplifyMatrix = FALSE
+    )
+}
