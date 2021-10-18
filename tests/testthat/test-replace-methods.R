97fe7014f09eaaca3e22e766ab0308bd440327ef
J. Allen Baron <jabaron.phd@gmail.com>
2021-10-18 11:53:29 -0400

    Add tests for replace_null() [FAIL - partial]
    Doesn't appear to work for NULL values in empty nested lists
    (i.e. list()).

--- tests/testthat/test-replace-methods.R
+++ tests/testthat/test-replace-methods.R
@@ -29,3 +29,66 @@ test_that("replace_na.list() works for nested lists", {
         list(1, A = "a", B = list("b", 1), list(C = c(3, 1)))
     )
 })
+
+
+# NULL --------------------------------------------------------------------
+
+test_that("replace_null() works for un-nested lists", {
+    l2 <- list(NULL, A = "a", B = "b", Z = NULL)
+
+    # chr
+    expect_identical(
+        replace_null(l2, "Y!"),
+        list("Y!", A = "a", B = "b", Z = "Y!")
+    )
+    # dbl
+    expect_identical(
+        replace_null(l2, 1),
+        list(1, A = "a", B = "b", Z = 1)
+    )
+    # NA
+    expect_identical(
+        replace_null(l2, NA),
+        list(NA, A = "a", B = "b", Z = NA)
+    )
+})
+
+test_that("replace_null() works for nested lists", {
+    l3 <- list(NULL, A = "a", Z = NULL, L1 = list(C = 3, Z = NULL))
+
+    # chr
+    expect_identical(
+        replace_null(l3, "Y!"),
+        list("Y!", A = "a", Z = "Y!", L1 = list(C = 3, Z = "Y!"))
+    )
+    # dbl
+    expect_identical(
+        replace_null(l3, 1),
+        list(1, A = "a", Z = 1, L1 = list(C = 3, Z = 1))
+    )
+    # NA
+    expect_identical(
+        replace_null(l3, NA),
+        list(NA, A = "a", Z = NA, L1 = list(C = 3, Z = NA))
+    )
+})
+
+test_that("replace_null() adds replace value to empty lists", {
+    l3 <- list(list())
+
+    # chr
+    expect_identical(
+        replace_null(l3, "Y!"),
+        list(list("Y!"))
+    )
+    # dbl
+    expect_identical(
+        replace_null(l3, 1),
+        list(list(1))
+    )
+    # NA
+    expect_identical(
+        replace_null(l3, NA),
+        list(list(NA))
+    )
+})
