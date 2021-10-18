51c05575339053671919f84da4f5a870d061df1b
J. Allen Baron <jabaron.phd@gmail.com>
2021-10-18 16:47:44 -0400

    Use jsonlite serialize method for list to json [PASS]
    UPDATE:
    jsonlite has the more verbose (un)serialize method for conversion
    of R objects to JSON and it works perfectly for all tests without
    any need for NULL/NA conversion. The only downside is it is fairly
    verbose but I don't anticipate that being a problem since these
    data will likely not be read directly by a person.

--- R/confine_list.R
+++ R/confine_list.R
@@ -1,39 +1,27 @@
 #' Convert (Nested) List to/from Character Vector
 #'
 #' These functions convert a (nested) list to or from a character vector of the
-#' same length, preserving or restoring the list structure and values with JSON
-#' strings. They are primarily designed to preserve the structure of simple
-#' nested lists (no internal data.frames, matrices, or arrays), as would result
-#' from an API call converted to a list without any conversion
-#' (e.g. flattening).
+#' same length, preserving and restoring the list structure and values using
+#' JSON strings. They do not perform _any_ flattening or simplification and the
+#' JSON strings are verbose.
 #'
-#' @section NOTE:
-#' These could potentially be made to handle more diverse lists by including
-#' options that control how data.frames & matrices are managed, ensuring
-#' to/from procedures are linked and occur in predictable ways (see
-#' arguments for [jsonlite::to/fromJSON](jsonlite::fromJSON) for details).
-#'
-#' @param .list a (nested) list
-#' @param .json_chr a character vector containing JSON representation of list
-#' elements below the top level
-#' @inheritParams RJSONIO::fromJSON
+#' @param .list A (nested) list.
+#' @param .json_chr A character vector containing the JSON representation of
+#' (nested) list elements below the top level.
 #'
 #' @export
 confine_list <- function(.list) {
     purrr::map_chr(
         .list,
-        RJSONIO::toJSON,
-        asIs = TRUE
+        jsonlite::serializeJSON
     )
 }
 
 #' @rdname confine_list
 #' @export
-release_list <- function(.json_chr, nullvalue = NA) {
+release_list <- function(.json_chr) {
     purrr::map(
         .json_chr,
-        RJSONIO::fromJSON,
-        nullvalue = nullvalue,
-        simplify = TRUE
+        jsonlite::unserializeJSON
     )
 }
