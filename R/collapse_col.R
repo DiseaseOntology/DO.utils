9c2baa35d859d88d94cb920c04898b3060ea03a3
J. Allen Baron <jabaron.phd@gmail.com>
2021-10-26 13:51:32 -0400

    Allow collapse method to differ by column
    INCLUDES:
    - Added collapse methods & method constructor function
    - Updated documentation
    - WORKS for list(col = method, ...)
    
    PROBLEM:
    Examples DO NOT WORK, even when wrapped in lists; partly due to
    premature execution when bare column names are passed but it
    also does not work for quoted names.

--- R/collapse_col.R
+++ R/collapse_col.R
@@ -1,11 +1,12 @@
 #' Collapse Column(s) Flexibly
 #'
-#' Collapses values in the column(s) specified, concatenating unique values in
-#' those columns for each record (where a record is defined as a unique
-#' row including all columns _NOT_ specified in `collapse`).
+#' Collapses values in the column(s) specified using a defined method for each
+#' record, where a record is defined as a unique observation comprised of all
+#' columns _NOT_ specified in `collapse`).
 #'
-#' @param df a data.frame
-#' @param collapse the name of the column in the data.frame to collapse
+#' @param df A data.frame
+#' @param collapse A list of columns to collapse using `method` or a named
+#'     list specifying the method for each column, e.g. column_name = method.
 #' @inheritParams vctr_to_string
 #'
 #' @examples
@@ -24,19 +25,53 @@
 #' collapse_col_flex(cc_df, c(x, z))
 #'
 #' @export
-collapse_col_flex <- function(df, collapse, delim = "|") {
+collapse_col_flex <- function(df, collapse = list(), method = "unique",
+                         delim = "|") {
+
+    # validate arguments
+    valid_methods <- c("unique", "first", "last")
+    assertthat::assert_that(rlang::is_list(collapse))
+    method <- match.arg(method, choices = valid_methods)
+
+    if (!is.null(names(collapse))) {
+        # validate collapse argument further if named list
+        assertthat::assert_that(all(names(collapse) %in% names(df)))
+        assertthat::assert_that(
+            all(purrr::map_int(collapse, length) == 1),
+            all(collapse %in% valid_methods)
+        )
+
+        collapse_vars <- names(collapse)
+        method <- collapse
+    } else {
+        assertthat::assert_that(all(collapse %in% names(df)))
+        collapse_vars <- collapse
+    }
+
     df %>%
-        dplyr::group_by(dplyr::across(-{{ collapse }})) %>%
+        dplyr::group_by(dplyr::across(-{{ collapse_vars }})) %>%
         dplyr::summarize(
             dplyr::across(
-                .cols = {{ collapse }},
-                .fns = ~ vctr_to_string(
-                    unique(.x),
+                .cols = {{ collapse_vars }},
+                .fns = ~ collapse_method(
+                    .x,
+                    method[[dplyr::cur_column()]],
                     delim = delim
                 )
             )
         ) %>%
         dplyr::ungroup()
+}
+
+# collapse_col_flex() helper (internal)
+collapse_method <- function(.col, method = "unique", delim = "|") {
+    method_fxn <- list(
+        unique = unique,
+        first = dplyr::first,
+        last = dplyr::last
+    )
+
+    vctr_to_string(method_fxn[[method]](.col), delim = delim)
 }
 
 
