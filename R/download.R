f949d730a4995d4966c20e6632eb35379118bc07
J. Allen Baron <jabaron.phd@gmail.com>
2021-11-05 18:24:41 -0400

    Delete check_dl_status()
    Replaced by download_status Reference Class

--- R/download.R
+++ R/download.R
@@ -40,44 +40,6 @@ download_file <- function(url, dest_file, on_failure = "warn", ...) {
 }
 
 
-#' Check Exit Status of Downloads (Internal)
-#'
-#' Check the return value when [utils::download.file()] is used, as recommended
-#' in the "Good practice" section of it's documentation.
-#'
-#' @param exit_code An integer vector of exit code(s) produced by
-#'     [utils::download.file()].
-#' @param dest_file A character vector of relative or absolute paths to
-#'     downloaded files. Used to make output and messages more informative; the
-#'     existence of these files is not tested.
-#' @param abort A logical scalar indicating whether to abort when non-zero
-#'     exit codes are produced. When `FALSE`, (default) failed downloads will
-#'     produce warnings. `TRUE` will likely only be useful for single file
-#'     downloads.
-#'
-#' @return
-#' A character vector of paths to destination files that downloaded
-#' successfully, with a warning indicating which fail when `abort = FALSE`
-#' (default).
-check_dl_status <- function(exit_code, dest_file, abort = FALSE) {
-    assert_scalar_logical(abort)
-    failed <- exit_code != 0
-    if (any(failed)) {
-        msg = c("File(s) failed to download",
-                paste0(dest_file[failed], " - exit code ", exit_code[failed])
-        )
-        if (abort) {
-            rlang::inform(
-                c("File(s) downloaded successfully:", dest_file[!failed])
-            )
-            rlang::abort(message = msg)
-        }
-        rlang::warn(message = msg)
-    }
-    dest_file[!failed]
-}
-
-
 #' A Reference Class to represent file download status.
 #'
 #' @field successful A character vector for paths of successfully downloaded
