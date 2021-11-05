4901a5f97928cffd301a4234583a7e6473756961
J. Allen Baron <jabaron.phd@gmail.com>
2021-11-05 14:50:52 -0400

    Create download_file()

--- R/download.R
+++ R/download.R
@@ -1,3 +1,33 @@
+#' Download File(s) from the Internet
+#'
+#' `download_file()` is a vectorized version of [utils::download.file()] that
+#' does not rely on specifying the "libcurl" method for vectorization.
+#'
+#' @param url A character vector naming the URL of resource(s) to be downloaded.
+#' @param dest_file A character vector with the file path(s) where downloaded
+#'     file(s) will be saved.
+#' @inheritParams check_dl_status
+#' @param ... Additional arguments passed on to [download.file()].
+#'
+#' @return
+#' A character vector of paths to destination files that downloaded
+#' successfully, with a warning indicating which failed when `abort = FALSE`
+#' (default).
+#'
+#' @export
+download_file <- function(url, dest_file, abort = FALSE, ...) {
+    assertthat::assert_that(length(dest_file) == length(url))
+
+    exit_code <- purrr::map2_int(
+        .x = url,
+        .y = dest_file,
+        ~ download.file(.x, .y, ...)
+    )
+
+    check_dl_status(exit_code, dest_file, abort = abort)
+}
+
+
 #' Check Exit Status of Downloads (Internal)
 #'
 #' Check the return value when [utils::download.file()] is used, as recommended
