1cf08be120f6e2bf14b81c41e57164f5062824ac
J. Allen Baron <jabaron.phd@gmail.com>
2021-11-12 17:12:34 -0500

    Revert "Replace nlm_catalog with data from dump"
    This reverts commit 9d0c994b58895c8c903c6060f3a0eb9d42502f4f,
    in preparation for a different search.

--- data-raw/nlm_catalog.R
+++ data-raw/nlm_catalog.R
@@ -1,34 +1,40 @@
 ## code to prepare `nlm_catalog` dataset goes here
 
 library(tidyverse)
+library(rentrez)
+library(keyring)
 
-# Dump of PubMed and NCBI molecular biology database journals from NLM Catalog
-# at https://pubmed.ncbi.nlm.nih.gov/help/#journal-lists
-nlm_catalog_txt <- readr::read_lines(
-    "https://ftp.ncbi.nih.gov/pubmed/J_Entrez.gz"
+rentrez::set_entrez_key(keyring::key_get("ENTREZ_KEY"))
+
+nlm_catalog_id <- rentrez::entrez_search(
+    db = "nlmcatalog",
+    term = "currentlyindexed",
+    use_history = TRUE
 )
 
-extract_field <- function(x, field) {
-    x[stringr::str_detect(x, field)] %>%
-        stringr::str_remove(paste0(field, ":")) %>%
-        stringr::str_trim()
-}
+nlm_catalog_summary <- rentrez::entrez_summary(
+    db = "nlmcatalog",
+    web_history = nlm_catalog_id$web_history,
+    always_return_list = TRUE,
+    retmode = "xml"
+)
 
-nlm_catalog_complete <- tibble::tibble(
-    iso_abbr = extract_field(nlm_catalog_txt, "IsoAbbr"),
-    issn_online = extract_field(nlm_catalog_txt, "ISSN \\(Online\\)"),
-    issn_print = extract_field(nlm_catalog_txt, "ISSN \\(Print\\)"),
-    title = extract_field(nlm_catalog_txt, "JournalTitle"),
-    id = extract_field(nlm_catalog_txt, "JrId"),
-    med_abbr = extract_field(nlm_catalog_txt, "MedAbbr"),
-    nlm_id = extract_field(nlm_catalog_txt, "NlmId")
-) %>%
-    dplyr::mutate(dplyr::across(dplyr::everything(), replace_blank))
+nlm_catalog_complete <- as_tibble(nlm_catalog_summary) %>%
+    dplyr::mutate(
+        dplyr::across(where(rlang::is_list), confine_list)
+    )
 
 readr::write_csv(nlm_catalog_complete, "data-raw/nlm_catalog-complete.csv")
 
-# iso_abbr == med_abbr
 nlm_catalog <- nlm_catalog_complete %>%
-    dplyr::select(title, abbrev_title = med_abbr)
+    dplyr::mutate(TitleMainList = release_list(TitleMainList)) %>%
+    tidyr::hoist(
+        .col = TitleMainList,
+        Title = list(1L, "Title")
+    ) %>%
+    dplyr::select(title = Title, abbrev_title = MedlineTA) %>%
+    dplyr::mutate(
+        dplyr::across(dplyr::everything(), ~stringr::str_remove(.x, "\\.$"))
+    )
 
 readr::write_csv(nlm_catalog, "data-raw/nlm_catalog.csv")
