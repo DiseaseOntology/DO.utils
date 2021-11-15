703d87290d28853041124232dd5d5091be6c97c1
J. Allen Baron <jabaron.phd@gmail.com>
2021-11-15 11:13:09 -0500

    Get more complete nlm_catalog
    Includes all NLM records with abbreviations from 2009-2021.
    
    May be more limited than previous search for those "currently
    indexed by Medline" despite having far more records.
    
    REMAINS INSUFFICIENT.

--- data-raw/nlm_catalog.R
+++ data-raw/nlm_catalog.R
@@ -1,4 +1,4 @@
-## code to prepare `nlm_catalog` dataset goes here
+## prepare `nlm_catalog` dataset
 
 library(tidyverse)
 library(rentrez)
@@ -6,33 +6,57 @@ library(keyring)
 
 rentrez::set_entrez_key(keyring::key_get("ENTREZ_KEY"))
 
+
+# Download & save complete NLM catalog from 2009-2021 ---------------------
+
 nlm_catalog_id <- rentrez::entrez_search(
     db = "nlmcatalog",
-    term = "currentlyindexed",
+    term = "2009:2021[Publication Year]",
     use_history = TRUE
 )
 
-nlm_catalog_summary <- rentrez::entrez_summary(
-    db = "nlmcatalog",
-    web_history = nlm_catalog_id$web_history,
-    always_return_list = TRUE,
-    retmode = "xml"
+retstart_pos <- seq(1, nlm_catalog_id$count, by = 10000)
+nlm_catalog_summary_list <- purrr::map(
+    retstart_pos,
+    function(rs) {
+        tryCatch(
+            rentrez::entrez_summary(
+                db = "nlmcatalog",
+                web_history = nlm_catalog_id$web_history,
+                always_return_list = TRUE,
+                retmode = "xml",
+                retstart = rs,
+                retmax = 10000
+            ),
+            error = function(e) {
+                list(
+                    error_msg = conditionMessage(e),
+                    retstart = rs
+                )
+            }
+        )
+    }
 )
 
-nlm_catalog_complete <- as_tibble(nlm_catalog_summary) %>%
-    dplyr::mutate(
-        dplyr::across(where(rlang::is_list), confine_list)
-    )
+nlm_catalog_complete <- purrr::map_dfr(nlm_catalog_summary_list, as_tibble)
 
-readr::write_csv(nlm_catalog_complete, "data-raw/nlm_catalog-complete.csv")
+save(
+    nlm_catalog_complete,
+    file = "data-raw/nlm_catalog_complete.rda",
+    compress = "xz"
+)
+
+
+# Create titles/abbreviation catalog for use by DO.utils ------------------
 
 nlm_catalog <- nlm_catalog_complete %>%
-    dplyr::mutate(TitleMainList = release_list(TitleMainList)) %>%
     tidyr::hoist(
         .col = TitleMainList,
-        Title = list(1L, "Title")
+        title = list(1L, "Title")
     ) %>%
-    dplyr::select(title = Title, abbrev_title = MedlineTA) %>%
+    # reduce list to those titles with abbreviations
+    dplyr::filter(!is.na(MedlineTA)) %>%
+    dplyr::select(title, abbrev_title = MedlineTA) %>%
     dplyr::mutate(
         dplyr::across(dplyr::everything(), ~stringr::str_remove(.x, "\\.$"))
     )
