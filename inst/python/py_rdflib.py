ea6f60a1192d0fde41acbd250f66713d87cd26d4
J. Allen Baron <jabaron.phd@gmail.com>
2022-01-13 13:56:29 -0500

    Create python module py_rdflib
    Script copied from DO_dev work and intended for import as module.
    Defines functions:
    - read_rdf() to load RDF files
    - sparql_query() to query files using SPARQL
    
    Functions internally wrap python rdflib module.

---
+++ inst/python/py_rdflib.py
@@ -0,0 +1,40 @@
+# Enable python SPARQL queries
+
+import rdflib
+import pandas as pd
+from os.path import exists
+
+
+def read_rdf(path):
+    """
+    Read RDF/OWL files into Python for querying/manipulation.
+
+    Keyword arguments:
+    path -- the path to the RDF/OWL file
+    """
+    g = rdflib.Graph()
+    g.parse(path, format = "application/rdf+xml")
+    return g
+
+
+def sparql_query(rdf_graph, query):
+    """
+    Query a loaded RDF graph with SPARQL.
+
+    Keyword arguments:
+    rdf_graph -- the RDF graph object
+    query -- the SPARQL query, as a string or the path to a sparql file
+    """
+    if not isinstance(query, str):
+        raise TypeError('query must be a string/path.')
+
+    if exists(query):
+        with open(query) as f:
+        q = f.read()
+    else:
+        q = query
+
+    res = rdf_graph.query(q)
+    df = pd.DataFrame(res, columns = res.vars)
+    # df = df_raw.convert_dtypes()
+    return df
