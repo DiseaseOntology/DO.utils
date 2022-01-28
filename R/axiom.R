3e9dc81ea2d423c7c2454fc1355b1ad9ba491097
J. Allen Baron <jabaron.phd@gmail.com>
2022-01-28 14:56:51 -0500

    Add extract_subclass_axiom()
    To extract subClassOf axioms from doid-edit.owl. No parsing
    functionality.

--- R/axiom.R
+++ R/axiom.R
@@ -2,3 +2,8 @@ extract_eq_axiom <- function(DO_repo) {
     doid_edit <- read_doid_edit(DO_repo)
     grep("EquivalentClasses", doid_edit, value = TRUE)
 }
+
+extract_subclass_axiom <- function(DO_repo) {
+    doid_edit <- read_doid_edit(DO_repo)
+    grep("SubClassOf.*Object", doid_edit, value = TRUE)
+}
