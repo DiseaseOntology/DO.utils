60771101a90305649e2900726bf97b3b85d76877
J. Allen Baron <jabaron.phd@gmail.com>
2022-01-28 15:09:30 -0500

    Add extract_class_axiom()
    To extract all class axioms (equivalentClass and subClassOf).

--- R/axiom.R
+++ R/axiom.R
@@ -1,3 +1,23 @@
+#' Extract Class Axioms
+#'
+#' Extract `owl:equivalentClass` and `owl:subClassOf` axioms from the
+#' doid-edit.owl file.
+#'
+#' @inheritParams read_doid_edit
+#'
+#' @returns
+#' A list of two character vectors (`eq` and `subclass`) containing axioms in
+#' OWL functional format.
+#'
+#' @export
+extract_class_axiom <- function(DO_repo) {
+    doid_edit <- read_doid_edit(DO_repo)
+    eq_raw <- grep("EquivalentClasses", doid_edit, value = TRUE)
+    subclass_raw <- grep("SubClassOf.*Object", doid_edit, value = TRUE)
+    list(eq = eq_raw, subclass = subclass_raw)
+}
+
+
 #' Extract Equivalent Class Axioms
 #'
 #' Extract `owl:equivalentClass` axioms from the doid-edit.owl file.
