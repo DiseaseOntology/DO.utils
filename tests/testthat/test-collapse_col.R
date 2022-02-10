0bfb1dc6e409b7acc951b830fa6b04271070e032
J. Allen Baron <jabaron.phd@gmail.com>
2022-02-10 12:38:40 -0500

    Create tests for collapse_col() [PASS]

--- tests/testthat/test-collapse_col.R
+++ tests/testthat/test-collapse_col.R
@@ -4,6 +4,45 @@ cc_df <- tibble::tibble(
      z = c("Z", "Y", "X", "X", "W", "V", "U")
 )
 
+# collapse_col() ----------------------------------------------------------
+
+test_that("collapse_col() works", {
+    # expected results from collapsing specified column(s)
+    x_collapse <- tibble::tibble(
+        # NOTE:
+        #   1. collapsed cols converted to chr (even if nothing to paste together).
+        #   2. group_by reorders by uncollapsed cols - combination of y + z.
+        x = c("2", "1", "3", "4", "4", "4"),
+        y = c("a", "a", "b", "c", "c", "e"),
+        z = c("Y", "Z", "X", "V", "W", "U")
+    )
+
+    z_collapse <- tibble::tibble(
+        x = c(1, 2, 3, 4, 4),
+        y = c("a", "a", "b", "c", "e"),
+        z = c("Z", "Y", "X", "W|V", "U")
+    )
+
+    xz_collapse <- tibble::tibble(
+        x = c("1|2", "3", "4", "4"),
+        y = c("a", "b", "c", "e"),
+        z = c("Z|Y", "X", "W|V", "U")
+    )
+
+    # tests
+    expect_identical(collapse_col(cc_df, x), x_collapse)
+    expect_identical(collapse_col(cc_df, z), z_collapse)
+    expect_identical(collapse_col(cc_df, c(x, z)), xz_collapse)
+})
+
+test_that("collapse_col(): duplicated rows (3-4) are collapsed with any
+          column specified", {
+              res <- collapse_col(cc_df, y)
+              expect_equal(nrow(res), nrow(cc_df) - 1)
+              expect_false(any(all_duplicated(res)))
+          })
+
+
 # collapse_col_flex() -----------------------------------------------------
 
 test_that("all collapse_col_flex() methods works", {
