11245f7665963062245eda7d5db1f8af38981286
J. Allen Baron <jabaron.phd@gmail.com>
2022-02-10 11:36:12 -0500

    Copy collapse_col() as collapse_col_flex()
    In preparation for making more flexible (and probably complex)
    version.

--- R/collapse_col.R
+++ R/collapse_col.R
@@ -1,3 +1,45 @@
+#' Collapse Column(s) Flexibly
+#'
+#' Collapses values in the column(s) specified, concatenating unique values in
+#' those columns for each record (where a record is defined as a unique
+#' row including all columns _NOT_ specified in `.cols`).
+#'
+#' @param df a data.frame
+#' @param .cols the name of the column in the data.frame to collapse
+#' @inheritParams vctr_to_string
+#'
+#' @examples
+#' cc_df <- tibble::tibble(
+#'      x = c(1, 2, 3, 3, 4, 4, 4),
+#'      y = c("a", "a", "b", "b", "c", "c", "e"),
+#'      z = c("Z", "Y", "X", "X", "W", "V", "U")
+#' )
+#' cc_df
+#'
+#' # completely duplicated rows (3-4) are collapsed with any .cols specified
+#' collapse_col_flex(cc_df, y)
+#'
+#' collapse_col_flex(cc_df, x)
+#' collapse_col_flex(cc_df, z)
+#' collapse_col_flex(cc_df, c(x, z))
+#'
+#' @export
+collapse_col_flex <- function(df, .cols, delim = "|") {
+    df %>%
+        dplyr::group_by(dplyr::across(-{{ .cols }})) %>%
+        dplyr::summarize(
+            dplyr::across(
+                .cols = {{ .cols }},
+                .fns = ~ vctr_to_string(
+                    unique(.x),
+                    delim = delim
+                )
+            )
+        ) %>%
+        dplyr::ungroup()
+}
+
+
 #' Collapse Column(s)
 #'
 #' Collapses values in the column(s) specified, concatenating unique values in
