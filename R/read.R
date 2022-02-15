a982978f27aff2bee8d501f722454cb9dc7c173f
J. Allen Baron <jabaron.phd@gmail.com>
2022-02-15 15:58:19 -0500

    Set class on doid-edit.owl read object

--- R/read.R
+++ R/read.R
@@ -9,5 +9,6 @@ read_doid_edit <- function(DO_repo) {
     doid_edit_path <- file.path(DO_repo, "src", "ontology", "doid-edit.owl")
     doid_edit <- readr::read_lines(doid_edit_path)
 
+    class(doid_edit) <- c("doid_edit", class(doid_edit))
     doid_edit
 }
