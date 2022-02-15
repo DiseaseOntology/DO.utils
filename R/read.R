a5d810a4481c417b4828f9451e45559dd789d336
J. Allen Baron <jabaron.phd@gmail.com>
2022-02-15 15:32:49 -0500

    Document read_doid_edit()

--- R/read.R
+++ R/read.R
@@ -1,3 +1,10 @@
+#' Read doid-edit.owl (INTERNAL)
+#'
+#' Read the doid-edit.owl file from a local copy of the Human Disease Ontology
+#' Github repo.
+#'
+#' @param DO_repo The local path to the `HumanDiseaseOntology` repo, as a
+#'     string.
 read_doid_edit <- function(DO_repo) {
     doid_edit_path <- file.path(DO_repo, "src", "ontology", "doid-edit.owl")
     doid_edit <- readr::read_lines(doid_edit_path)
