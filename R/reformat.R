37399fb0ed587f952b97ea187d63b6b3ac072340
J. Allen Baron <jabaron.phd@gmail.com>
2022-03-10 13:01:54 -0500

    Loosen requirement to optionally allow_bare numbers

--- R/reformat.R
+++ R/reformat.R
@@ -1,11 +1,13 @@
 #' Reformat DOIDs
 #'
-#' Convert valid DOIDs or bare numbers to a specified DOID format. No attempt is
-#' made to validate bare numbers as DOIDs.
+#' Convert a mix of valid DOIDs and/or bare numbers to a specified DOID format.
+#' No attempt is made to validate bare numbers as true DOIDs.
 #'
 #' @inheritParams is_valid_doid
 #' @param to The format to convert the DOIDs to, as a string. All valid formats
 #'     are possible options: "CURIE" (default), "URI", "obo_CURIE", "basename".
+#' @param allow_bare Whether bare numbers should be allowed as input, `TRUE`
+#'     or `FALSE` (default).
 #'
 #' @examples
 #' x <- c(
@@ -20,11 +22,12 @@
 #' reformat_doid(x, to = "obo_CURIE")
 #' reformat_doid(x, to = "basename")
 #'
+#' w_bare <- c(x, "0001816")
+#' reformat_doid(w_bare, allow_bare = TRUE)
+#'
 #' @export
-reformat_doid <- function(x, to = "CURIE") {
-    assertthat::assert_that(all(is_valid_doid(x)))
+reformat_doid <- function(x, to = "CURIE", allow_bare = FALSE) {
     to <- match.arg(to, choices = c("URI", "CURIE", "obo_CURIE", "basename"))
-
     prefix <- switch(
         to,
         URI = "http://purl.obolibrary.org/obo/DOID_",
@@ -33,7 +36,19 @@ reformat_doid <- function(x, to = "CURIE") {
         basename = "DOID_"
     )
 
-    reformatted <- stringr::str_replace(x, "^.*DOID[:_]", prefix)
+    if (allow_bare) {
+        bare_number <- stringr::str_detect(x, "^[0-9]{1,7}$")
+        assertthat::assert_that(all(is_valid_doid(x) | bare_number))
+
+        reformatted <- dplyr::if_else(
+            bare_number,
+            paste0(prefix, x),
+            stringr::str_replace(x, "^.*DOID[:_]", prefix)
+        )
+    } else {
+        assertthat::assert_that(all(is_valid_doid(x)))
+        reformatted <- stringr::str_replace(x, "^.*DOID[:_]", prefix)
+    }
 
     reformatted
 }
--- tests/testthat/test-reformat.R
+++ tests/testthat/test-reformat.R
@@ -1,23 +1,28 @@
+# input & expected output
 x <- c("http://purl.obolibrary.org/obo/DOID_0001816", "DOID:4",
        "obo:DOID_14566", "DOID_0040001")
 
-test_that("reformat_doid() works", {
-    uri <- c("http://purl.obolibrary.org/obo/DOID_0001816",
-             "http://purl.obolibrary.org/obo/DOID_4",
-             "http://purl.obolibrary.org/obo/DOID_14566",
-             "http://purl.obolibrary.org/obo/DOID_0040001")
-    curie <- c("DOID:0001816", "DOID:4", "DOID:14566", "DOID:0040001")
-    obo_curie <- c("obo:DOID_0001816", "obo:DOID_4", "obo:DOID_14566",
-                   "obo:DOID_0040001")
-    basename <- c("DOID_0001816", "DOID_4", "DOID_14566", "DOID_0040001")
+curie <- c("DOID:0001816", "DOID:4", "DOID:14566", "DOID:0040001")
+uri <- c("http://purl.obolibrary.org/obo/DOID_0001816",
+         "http://purl.obolibrary.org/obo/DOID_4",
+         "http://purl.obolibrary.org/obo/DOID_14566",
+         "http://purl.obolibrary.org/obo/DOID_0040001")
+obo_curie <- c("obo:DOID_0001816", "obo:DOID_4", "obo:DOID_14566",
+               "obo:DOID_0040001")
+basename <- c("DOID_0001816", "DOID_4", "DOID_14566", "DOID_0040001")
 
+# tests
+test_that("reformat_doid() works", {
     expect_identical(reformat_doid(x, to = "URI"), uri)
     expect_identical(reformat_doid(x, to = "CURIE"), curie)
     expect_identical(reformat_doid(x, to = "obo_CURIE"), obo_curie)
     expect_identical(reformat_doid(x, to = "basename"), basename)
 })
 
-test_that("reformat_doid() fails if any invalid DOIDs", {
-    x[1] <- "0001816"
-    expect_error(reformat_doid(x, to = "URI"))
+test_that("reformat_doid() allow_bare switch works", {
+    w_bare <- c(x, "0001816")
+    bare_expected <- c(curie, "DOID:0001816")
+
+    expect_error(reformat_doid(w_bare))
+    expect_identical(reformat_doid(w_bare, allow_bare = TRUE), bare_expected)
 })
