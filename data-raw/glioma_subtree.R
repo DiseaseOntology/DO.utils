1d10a4a080e48f8eeeb570d37683b70bc0aaba3a
J. Allen Baron <jabaron.phd@gmail.com>
2022-03-23 01:03:55 -0400

    Add missing parent needed for test data

--- data-raw/glioma_subtree.R
+++ data-raw/glioma_subtree.R
@@ -12,7 +12,7 @@ df <- extract_subtree(repo$doid, top_node = "DOID:3070")
 readr::write_csv(df, "inst/extdata/glioma_subtree.csv")
 
 # simplify & save for testing
-include <- c("DOID:3070", "DOID:5074", "DOID:5503", "DOID:7788")
+include <- c("DOID:3070", "DOID:5074", "DOID:5503", "DOID:7788", "DOID:3185")
 test_df <- dplyr::filter(df, id %in% include)
 
 readr::write_csv(test_df, "tests/testthat/data/format_subtree.csv")
