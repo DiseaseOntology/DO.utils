32e4eb52af320ddd3ca9afe633c9da8f7f7b28b4
J. Allen Baron <jabaron.phd@gmail.com>
2022-04-05 17:22:25 -0400

    Update documentation for internal functions

--- R/parse.R
+++ R/parse.R
@@ -1,12 +1,16 @@
-#' Parse URLs
+#' Parse URL (INTERNAL)
 #'
-#' Parses URLs into protocol, domain, and path components. From
-#' `robotstxt:::parse_url()` (MIT license, [copyright Peter
-#' Meissner](https://github.com/ropensci/robotstxt/blob/master/LICENSE)), with
-#' slight modification.
+#' Parses one or more URL(s) into protocol, domain, and path components.
+#'
+#' @section Provenance:
+#' This function is originally from `robotstxt:::parse_url()` (MIT license,
+#' [copyright Peter Meissner](https://github.com/ropensci/robotstxt/blob/master/LICENSE)),
+#' with slight modification.
+#'
+#' @param url One or more URLs, as a character vector.
 #'
 #' @keywords internal
-parse_url <- function (url) {
+parse_url <- function(url) {
     match <- stringr::str_match(string = url, pattern = "(^\\w+://)?([^/]+)?(/.*)?")
     match <- match[, -1, drop = FALSE]
     df <- tibble::as_tibble(
