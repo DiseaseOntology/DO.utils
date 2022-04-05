bbc10fdfc51360b0467912cbb87634854f66d1a5
J. Allen Baron <jabaron.phd@gmail.com>
2022-04-05 17:23:16 -0400

    Rename replace_www_duplicate() to standardize_www_duplicate()
    To better reflect its purpose... and document it.

--- R/validate_url.R
+++ R/validate_url.R
@@ -1,4 +1,13 @@
-replace_www_duplicate <- function(url, how = "remove") {
+#' Standardize "www" in URLs
+#'
+#' Standardizes "www" in URLs by removing or adding it when URLs of a domain
+#' exhibit mixed usage (some with, others without).
+#'
+#' @inheritParams parse_url
+#' @param how How "www" should be handled. One of "remove" (default) or "add".
+#'
+#' @keywords internal
+standardize_www_duplicate <- function(url, how = "remove") {
     how <- match.arg(how, c("remove", "add"))
     replace_domain <- switch(
         how,
