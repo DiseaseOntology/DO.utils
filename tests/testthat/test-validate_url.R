d20b4a2c8eff66e4a4531b5a3869c35053119778
J. Allen Baron <jabaron.phd@gmail.com>
2022-04-08 14:07:36 -0400

    Add tests for validate_url() [PASS]

---
+++ tests/testthat/test-validate_url.R
@@ -0,0 +1,31 @@
+test_that("validate_url() works", {
+    if(!curl::has_internet()) {
+        skip("No internet connection.")
+    }
+
+    res <- validate_url("disease-ontology.org")
+    expect_equal(
+        dplyr::select(res, -response),
+        tibble::tibble(
+            url = "disease-ontology.org",
+            status = "Success",
+            status_code = 200L,
+            redirect_url = "https://disease-ontology.org/"
+        )
+    )
+    expect_equal(class(res$response[[1]]), "response")
+})
+
+test_that("validate_url() works for R errors", {
+    expect_equal(
+        validate_url("https://this-url-doesnt-exist.none"),
+        tibble::tibble(
+            url = "https://this-url-doesnt-exist.none",
+            status = "R_error",
+            status_code = NA_integer_,
+            redirect_url = NA_character_,
+            exception = "Could not resolve host: this-url-doesnt-exist.none"
+        )
+    )
+})
+
