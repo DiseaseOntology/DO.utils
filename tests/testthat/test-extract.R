9bc134f968f20cf251004b49db15ffa1724d2efc
J. Allen Baron <jabaron.phd@gmail.com>
2022-04-08 15:32:09 -0400

    Add tests for extract_robots_delay() [MIXED]
    PROBLEM:
    Fails when DO.utils is used as user agent.

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -38,3 +38,22 @@ test_that("extract_pmid.elink_list does NOT produce condition when all elements 
         extract_pmid(elink_list_full, no_result = "error")
     )
 })
+
+
+# extract_robots_delay() tests --------------------------------------------
+
+# data
+rt_txt <- readr::read_file("data/fake_robots.txt")
+robxp <- spiderbar::robxp(rt_txt)
+
+# tests
+test_that("extract_robots_delay() uses '*'", {
+    expect_equal(extract_robots_delay(robxp, user_agent = "*"), 10)
+
+    # should use it if specified user agent is not in robots.txt
+    expect_equal(extract_robots_delay(robxp, user_agent = "fake"), 10)
+})
+
+test_that("extract_robots_delay() works for DO.utils", {
+    expect_equal(extract_robots_delay(robxp), 5)
+})
