3329323eb31c1cd06c3fc3f7cf1e756a63073515
J. Allen Baron <jabaron.phd@gmail.com>
2022-04-21 12:05:53 -0400

    Export standardize_www_duplicate()

--- R/standardize.R
+++ R/standardize.R
@@ -6,7 +6,7 @@
 #' @inheritParams parse_url
 #' @param how How "www" should be handled. One of "remove" (default) or "add".
 #'
-#' @keywords internal
+#' @export
 standardize_www_duplicate <- function(url, how = "remove") {
     how <- match.arg(how, c("remove", "add"))
     replace_domain <- switch(
