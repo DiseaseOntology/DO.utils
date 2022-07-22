ec52c564e40697ce6b25327dad80878f2fa16b8a
J. Allen Baron <jabaron.phd@gmail.com>
2022-07-22 16:06:22 -0400

    Create to_uri() with docs & test [PASS]

--- R/to.R
+++ R/to.R
@@ -42,3 +42,35 @@ to_curie <- function(x) {
         setNames(paste0(names(ns_prefix), ":"), ns_prefix)
     )
 }
+
+
+#' Convert CURIE to URI
+#'
+#' Converts CURIE(s) to URI(s).
+#'
+#' @param x CURIE(s), as a character vector.
+#'
+#' @section Note:
+#' Performs no CURIE validation but relies on a properly formatted CURIE for
+#' conversion (e.g. prefix:reference, according to the
+#' [CURIE standard](https://www.w3.org/TR/2010/NOTE-curie-20101216/#s_syntax)).
+#' Any inputs not matching a prefix of [ns_prefix] will be returned without
+#' modification.
+#'
+#' @examples
+#' .curie <- c("rdfs:comment", "dc:date", "terms:license", "owl:deprecated",
+#'             "oboInOwl:id", "UBERON:0000002", "DOID:0001816",
+#'             "doid:DO_AGR_slim")
+#' to_uri(.curie)
+#'
+#' #returns non-CURIE or unknown namespace prefixes unmodified
+#' to_curie(c("SYMP:0000000", "not a CURIE", "bioregistry.collection:0000001"))
+#'
+#' @family identifier converters
+#' @export
+to_uri <- function(x) {
+    stringr::str_replace_all(
+        x,
+        setNames(ns_prefix, paste0("^", names(ns_prefix), ":"))
+    )
+}
--- tests/testthat/test-to.R
+++ tests/testthat/test-to.R
@@ -1,31 +1,38 @@
+# Resource Identifier Converter Tests -------------------------------------
+
+uri <- c(
+    "http://purl.obolibrary.org/obo/SYMP_0000000",
+    "http://purl.obolibrary.org/obo/so#has_origin",
+    "http://www.geneontology.org/formats/oboInOwl#SubsetProperty",
+    "http://purl.org/dc/elements/1.1/date",
+    "http://www.w3.org/2002/07/owl#deprecated",
+    "http://purl.obolibrary.org/obo/UBERON_0000002",
+    "http://purl.obolibrary.org/obo/doid#DO_AGR_slim",
+    "http://purl.obolibrary.org/obo/DOID_0001816",
+    "http://purl.org/dc/terms/license",
+    "http://www.w3.org/2000/01/rdf-schema#comment",
+    "not a URI",
+    "http://bio2rdf.org/"
+)
+curie <- c(
+    "SYMP:0000000",
+    "obo:so#has_origin",
+    "oboInOwl:SubsetProperty",
+    "dc:date",
+    "owl:deprecated",
+    "UBERON:0000002",
+    "doid:DO_AGR_slim",
+    "DOID:0001816",
+    "terms:license",
+    "rdfs:comment",
+    "not a URI",
+    "http://bio2rdf.org/"
+)
+
 test_that("to_curie() works", {
-    uri <- c(
-        "http://purl.obolibrary.org/obo/SYMP_0000000",
-        "http://purl.obolibrary.org/obo/so#has_origin",
-        "http://www.geneontology.org/formats/oboInOwl#SubsetProperty",
-        "http://purl.org/dc/elements/1.1/date",
-        "http://www.w3.org/2002/07/owl#deprecated",
-        "http://purl.obolibrary.org/obo/UBERON_0000002",
-        "http://purl.obolibrary.org/obo/doid#DO_AGR_slim",
-        "http://purl.obolibrary.org/obo/DOID_0001816",
-        "http://purl.org/dc/terms/license",
-        "http://www.w3.org/2000/01/rdf-schema#comment",
-        "not a URI",
-        "http://bio2rdf.org/"
-    )
-    curie <- c(
-        "SYMP:0000000",
-        "obo:so#has_origin",
-        "oboInOwl:SubsetProperty",
-        "dc:date",
-        "owl:deprecated",
-        "UBERON:0000002",
-        "doid:DO_AGR_slim",
-        "DOID:0001816",
-        "terms:license",
-        "rdfs:comment",
-        "not a URI",
-        "http://bio2rdf.org/"
-    )
   expect_equal(to_curie(uri), curie)
+})
+
+test_that("to_uri() works", {
+    expect_equal(to_uri(curie), uri)
 })
