feaa262c0d222a7f56b12b068f22fab8b47ab703
J. Allen Baron <jabaron.phd@gmail.com>
2022-08-31 10:55:35 -0400

    Add format_axiom() with helpers
    To format OWL functional syntax EQ/SubClassOf axioms in more
    human readable way.

--- R/format.R
+++ R/format.R
@@ -95,3 +95,86 @@ format_subtree <- function(subtree_df, top_node) {
 
     formatted
 }
+
+
+#' Format Logical Axioms
+#'
+#' Format logical axioms in the style of Protege. Input axioms must be in OWL
+#' functional syntax.
+#'
+#' @param x Complete logical axioms in OWL functional syntax, as a character
+#'     vector.
+#' @param property_df \[Optional\] A data.frame consisting of relevant annotation and object
+#'     properties that may be used to make properties more readable in axioms.
+#'     If provided, data.frame should include two columns:
+#'
+#' * `property`: Each property's URI.
+#'
+#' * `label`: Each property's `rdfs:label` (or equivalent).
+#'
+#' _See 'Formatting Options' for details._
+#'
+#' @param generify_obo Whether to make OBO ontology classes and properties
+#'     generic, as `TRUE` or `FALSE` (default). _See 'Formatting Options' for_
+#'     _details._
+#' @param placeholders A set of 4 strings that will be used internally to
+#'     preserve axiom parentheses `[1:2]`, phrase spacing `[3]`, and complete
+#'     phrases `[4]`, as a character vector. These should not generally need to
+#'     be edited and are only exposed in case of conflicts within axioms.
+#' @param max_phrases The maximum number of phrases to update in an axiom,
+#'     as an integer. This protects against an infinite while loops and would
+#'     only need to be edited if one or more `Object*` OWL functional phrases
+#'     are returned unformatted.
+#'
+#' @section Formatting Options:
+#' `format_axiom()` will always rearrange equivalent class and subclass of
+#' logical axioms from OWL functional syntax to a more readable form, similar
+#' to Protege. However, classes and properties in axioms will be returned as
+#' URIs or CURIEs (as formatted in `x`). This may be desirable for further
+#' programmatic examination but will still be difficult for a human to read.
+#' To make them more readable, the `property_df` and `generify_obo` arguments
+#' can be used.
+#'
+#' `property_df` is used to replace OBO ontology property URIs or CURIEs
+#' with namespace-prefixed labels: e.g. `obo:RO_0004026` would become
+#' `RO:'disease has location'`. Non-OBO properties will not be modified.
+#'
+#' `generify_obo` replaces individual OBO URIs or CURIEs with either
+#' namespace-prefixed types, e.g. `obo:UBERON_0001032` would become
+#' `UBERON:anatomy`, or namespace only. This format simplifies axioms making
+#' basic analysis of axiom patterns easier. _See the documentation for_
+#' _[generify_obo()] for details._
+#'
+#' @export
+format_axiom <- function(x, property_df = NULL, generify_obo = FALSE,
+                         placeholders = c("<<<", ">>>", "%%%", "@@@"),
+                         max_phrases = 15L) {
+
+    out <- format_axiom_type(x)
+
+    i <- 0
+    while (any(has_object_stmt(out)) && i < max_phrases) {
+        out <- format_inmost_object_phrase(
+            out,
+            placeholders = placeholders
+        )
+        i <- i + 1
+    }
+
+    if (!is.null(property_df)) {
+        out <- label_properties(out, property_df)
+    }
+    if (isTRUE(generify_obo)) {
+        out <- generify_obo(out)
+    }
+
+    # Replace placeholders used to protect phrases, parentheses and spaces.
+    replacement <- purrr::set_names(
+        c("\\(", "\\)", " "),
+        nm = placeholders[1:3]
+    )
+    out <- stringr::str_replace_all(out, replacement)
+
+    out
+}
+
--- R/format_helpers.R
+++ R/format_helpers.R
@@ -160,3 +160,254 @@ fill_subclass <- function(subtree_df) {
 
     filled_df
 }
+
+
+# format_axiom() helpers --------------------------------------------------
+
+#' Label OWL Properties (INTERNAL)
+#'
+#' Replaces OWL annotation and object property URI/CURIEs defined by OBO
+#' ontologies with namespace-prefixed labels.
+#'
+#' @inheritParams format_axiom
+#'
+#' @family format_axiom() helpers
+#' @noRd
+label_properties <- function(x, property_df) {
+    stopifnot(all(c("property", "label") %in% names(property_df)))
+
+    obo_ns_pattern <- paste0("(", DO.utils::ns_prefix["obo"], "|obo:)")
+    ns_label <- paste0(
+        stringr::str_replace(
+            property_df$property,
+            ".+[/:]([A-Za-z]+)_[0-9]+",
+            "\\1:"
+        ),
+        "'", property_df$label, "'"
+    )
+    prop_replacement <- purrr::set_names(
+        ns_label,
+        nm = stringr::str_replace(
+            property_df$property,
+            obo_ns_pattern,
+            obo_ns_pattern
+        )
+    )
+
+    out <- stringr::str_replace_all(x, prop_replacement)
+    out
+}
+
+#' Generify OBO Classes/Properties (INTERNAL)
+#'
+#' [format_axiom()] internal helper that replaces URI/CURIEs defined by OBO
+#' ontologies with namespace-prefixed descriptions/types, where defined
+#' internally, or just the namespace. Will not modify non-OBO class URI/CURIEs.
+#'
+#' @inheritParams format_axiom
+#'
+#' @section Internally-defined OBO namespace-types:
+#' The following namespaces have 'namespace:types' defined by this function:
+#' * CL: 'CL:cell'
+#' * CHEBI: 'CHEBI:chemical'
+#' * DISDRIV: 'DISDRIV:disease_driver'
+#' * DOID: 'DOID:disease'
+#' * FOODON: 'FOODON:food_material'
+#' * ECO: 'ECO:evidence'
+#' * GENO: 'GENO:inheritance_pattern'
+#' * NCBITaxon: 'NCBITaxon:organism'
+#' * OMIM: 'OMIM:susceptibility'
+#' * UBERON: 'UBERON:anatomy'
+#' * UPHENO: 'UPHENO:phenotype'
+#' * SO: 'SO:sequence'
+#' * SYMP: 'SYMP:symptom'
+#' * TRANS: 'TRANS:pathogen_transmission'
+#'
+#' Ontologies with multiple object types used by the DO (e.g. HP, onset or
+#' phenotype), that are imported via DISDRIV (e.g. ExO), or used for their
+#' properties (e.g. RO) are not defined internally and will be formatted
+#' namespace only.
+#' .
+#'
+#' @family format_axiom() helpers
+#' @keywords internal
+generify_obo <- function(x) {
+    generic_ns_replacement <- c(
+        "UBERON" = "UBERON:anatomy", "CL" = "CL:cell",
+        "CHEBI" = "CHEBI:chemical", "DISDRIV" = "DISDRIV:disease_driver",
+        "DOID" = "DOID:disease", "FOODON" = "FOODON:food_material",
+        "ECO" = "ECO:evidence", "GENO" = "GENO:inheritance_pattern",
+        "NCBITaxon" = "NCBITaxon:organism", "OMIM" = "OMIM:susceptibility",
+        "UPHENO" = "UPHENO:phenotype", "SO" = "SO:sequence",
+        "SYMP" = "SYMP:symptom", "TRANS" = "TRANS:pathogen_transmission"
+    )
+
+    obo_ns_pattern <- paste0(
+        "(", DO.utils::ns_prefix["obo"], "|obo:)([A-Za-z]+)_[0-9]+"
+    )
+    out <- stringr::str_replace_all(x, obo_ns_pattern, "\\2")
+    out <- stringr::str_replace_all(out, generic_ns_replacement)
+    out
+}
+
+#' Formats Axiom Types (INTERNAL)
+#'
+#' Rearranges OWL equivalent class and subclass of types to human readable form,
+#' patterned after Protege's 'Usage' tab.
+#'
+#' @inheritParams format_axiom
+#'
+#' @family format_axiom() helpers
+#' @noRd
+format_axiom_type <- function(x) {
+    out <- stringr::str_replace(
+        x,
+        "(EquivalentClasses|SubClassOf)\\(([^ ]+) (.+)\\)",
+        "\\2 \\1 \\3"
+    )
+    out <- stringr::str_replace(out, "EquivalentClasses", "EquivalentTo")
+
+    out
+}
+
+#' Axiom Phrase Predicate (INTERNAL)
+#'
+#' Determines if OWL functional `Object.*` phrases exist in an axiom. Used to
+#' determine when all phrases have been reformatted.
+#'
+#' @inheritParams format_axiom
+#'
+#' @family format_axiom() helpers
+#' @noRd
+has_object_stmt <- function(x) {
+    stringr::str_detect(x, "ObjectSome|Object.+Cardinality|Object.+Of")
+}
+
+#' Format Inmost Axiom Phrase (INTERNAL)
+#'
+#' Extracts, formats, and replaces the inmost `Object.*` phrase of axioms.
+#' Formatting removes the `Object.*` identifier from the phrase, which prevents
+#' further modification. When used iteratively, this progressively formats
+#' phrases.
+#'
+#' **NOTE:** This is the only format_axiom() helper that uses `placeholder[4]`.
+#' `placeholder[1:3]` are passed along to internal functions.
+#'
+#' @inheritParams format_axiom
+#'
+#' @family format_axiom() helpers
+#' @noRd
+format_inmost_object_phrase <- function(x, placeholders) {
+    extract <- stringr::str_extract(x, "Object[^(]+\\([^()]+\\)")
+    phrase_placeholder <- placeholders[4]
+    hold <- stringr::str_replace(
+        x,
+        "Object[^(]+\\([^()]+\\)",
+        phrase_placeholder
+    )
+    mod <- format_object_phrase(extract, placeholders = placeholders[1:3])
+
+    purrr::map2_chr(
+        .x = hold,
+        .y = mod,
+        ~ stringr::str_replace(.x, phrase_placeholder, .y)
+    )
+}
+
+
+#' Format Object Phrase (INTERNAL)
+#'
+#' Workhorse responsible for formatting axiom phrases. Currently, can format
+#' 'some', 'IntersectionOf', 'UnionOf' and exact or minimum cardinality phrases.
+#'
+#' **NOTE:** `placeholder[1:3]` are passed along to internal functions.
+#'
+#' @inheritParams format_axiom
+#'
+#' @family format_axiom() > format_inmost_object_phrase() helpers
+#' @noRd
+format_object_phrase <- function(x, placeholders) {
+    some <- stringr::str_detect(x, "ObjectSome")
+    cardinal <- stringr::str_detect(x, "Object.+Cardinality")
+    join <- stringr::str_detect(x, "Object.+Of")
+
+    out <- dplyr::case_when(
+        some ~ format_some_phrase(x, placeholders = placeholders),
+        cardinal ~ format_cardinal_phrase(x, placeholders = placeholders),
+        join ~ expand_join_phrase(x, placeholders = placeholders)
+    )
+
+    out
+}
+
+#' Format 'some' Phrase (INTERNAL)
+#'
+#' @inheritParams format_axiom
+#'
+#' @family format_axiom() > format_inmost_object_phrase() > format_object_phrase() helpers
+#' @noRd
+format_some_phrase <- function(x, placeholders) {
+    replacement <- sandwich_text(
+        paste("\\1", "some", "\\2", sep = placeholders[3]),
+        placeholder = placeholders[1:2]
+    )
+    stringr::str_replace(
+        x,
+        "ObjectSomeValuesFrom\\(([^ ]+) (.+)\\)",
+        replacement = replacement
+    )
+}
+
+#' Format Min/Exact Cardinality Phrases (INTERNAL)
+#'
+#' @inheritParams format_axiom
+#'
+#' @family format_axiom() > format_inmost_object_phrase() > format_object_phrase() helpers
+#' @noRd
+format_cardinal_phrase <- function(x, placeholders) {
+    mutable_group <- sandwich_text("\\4", placeholders[1:2])
+    replacement <- paste(
+        "\\3", "\\1", "\\2", mutable_group,
+        sep = placeholders[3]
+    )
+
+    out <- stringr::str_replace(
+        x,
+        "Object([^C]+)Cardinality\\(([0-9]+) ([^ ]+) (.+)\\)",
+        replacement = replacement
+    )
+
+    mutable_pattern <- sandwich_text("([^ ]+)", placeholders[1:2])
+    out <- stringr::str_replace(out, mutable_pattern, "\\1")
+    out <- stringr::str_replace(out, " Exact ", " Exactly ")
+
+    out
+}
+
+#' Expands Intersection/UnionOf Phrases (INTERNAL)
+#'
+#' @inheritParams format_axiom
+#'
+#' @family format_axiom() > format_inmost_object_phrase() > format_object_phrase() helpers
+#' @noRd
+expand_join_phrase <- function(x, placeholders) {
+    type <- stringr::str_replace(x, "Object(.+)Of.*", "\\1")
+    jxn <- dplyr::recode(
+        type,
+        Union = sandwich_text("or", placeholders[3]),
+        Intersection = sandwich_text("and", placeholders[3])
+    )
+    out <- purrr::map2_chr(
+        .x = x,
+        .y = jxn,
+        ~ stringr::str_replace_all(
+            .x,
+            c("Object.+Of\\(" = placeholders[1],
+              " " = .y,
+              "\\)" = placeholders[2]
+            )
+        )
+    )
+
+    out
+}
