3d37c8cdcfa36e7103862a56cd5acb394b4dbc12
J. Allen Baron <jabaron.phd@gmail.com>
2022-08-31 11:01:35 -0400

    Add examine_snapshots()
    To simplify examining test snapshots. Currently only has method
    for format_axiom().

---
+++ R/utils-test.R
@@ -0,0 +1,61 @@
+#' Simplify Examining Test Snapshots (INTERNAL)
+#'
+#' Formats snapshots to make manual examination of results easier.
+#'
+#' @param fn_name The name of a function with snapshots to examine, as a string.
+#'
+#' @noRd
+examine_snapshots <- function(fn_name) {
+    stopifnot(rlang::is_string(fn_name))
+    fn <- switch(
+        fn_name,
+        format_axiom = examine_format_axiom_snapshots
+    )
+    fn()
+}
+
+#' DO NOT CALL DIRECTLY
+#'
+#' Produces df with:
+#' * `id`: which axiom
+#' * `cmd`: "original axiom" or cmd that produced result
+#' * `res`: original axiom or result
+#' * `paren_correct`: whether the correct number of parentheses are in a result
+#' * `paren`: number of opening parentheses in result, in case paren != correct
+#'
+#' @noRd
+examine_format_axiom_snapshots <- function(test_dir = "tests/testthat/") {
+
+    ax_df <- tibble::tibble(
+        id = 1:8,
+        cmd = "original axiom",
+        res = readr::read_lines(file.path(test_dir, "data/axioms.ofn")),
+        paren = stringr::str_count(res, "\\(")
+    )
+
+    ss <- readr::read_lines(file.path(test_dir, "_snaps/format.md"))
+    code_pos <- which(stringr::str_detect(ss, "Code")) + 1
+    res_pos <- which(stringr::str_detect(ss, "\\[[0-9]+\\]"))
+
+    ss_df <- tibble::tibble(
+        id = rep(1:8, 4),
+        cmd = stringr::str_squish(rep(ss[code_pos], each = 8)),
+        res = stringr::str_replace(ss[res_pos], '.*"(.+)".*', "\\1"),
+        paren = stringr::str_count(res, "\\(")
+    )
+
+    compare_df <- dplyr::bind_rows(ax_df, ss_df) %>%
+        dplyr::arrange(id, dplyr::desc(cmd)) %>%
+        dplyr::group_by(id) %>%
+        dplyr::mutate(
+            paren_correct = dplyr::if_else(
+                cmd == "original axiom",
+                NA,
+                paren[cmd == "original axiom"] - paren == 1
+            )
+        ) %>%
+        dplyr::ungroup()
+
+    compare_df
+}
+
