98e3ad6d5851234b3ead654076fb0cd211be7973
J. Allen Baron <jabaron.phd@gmail.com>
2022-09-22 18:28:11 -0400

    Make partition() docs findable with ?chunk
    Often easier to remember name chunk but don't want to rename it
    because of potential for conflict. "partition" is much less
    common than "chunk".

--- R/partition.R
+++ R/partition.R
@@ -1,8 +1,8 @@
 #' Partition vectors
 #'
-#' Divides vectors into partitions of a specified length and returns them as a
-#' list. If `x` is not completely divisible by `n` the last list item will have
-#' less than `n` elements.
+#' `partition` divides vectors into partitions of a specified length and returns
+#' them as a list. If `x` is not completely divisible by `n` the last list item
+#' will have less than `n` elements. Similar functions may be named "chunk".
 #'
 #' @param x A vector.
 #' @param n An integer specifying instances per partition.
@@ -15,6 +15,10 @@
 #' partition(letters[1:10], 5)
 #' partition(1:10, 3)
 #'
+#' @name chunk
+NULL
+
+#' @name chunk
 #' @export
 partition <- function(x, n) {
     pos <- seq_along(x)
