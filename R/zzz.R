0c21aaabb432cdf38cf55113872f0a4de5d21b71
J. Allen Baron <jabaron.phd@gmail.com>
2022-10-19 11:03:02 -0400

    Drop obvious non-cited by functions

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-pyobo_gutils <- NULL
-pyDOID <- NULL
-
-.onLoad <- function(libname, pkgname) {
-    # use superassignment to update global references to python modules
-    pyobo_gutils <<- reticulate::import("pyobo.gilda_utils", delay_load = TRUE)
-    pyDOID <<- reticulate::import("pyDOID", delay_load = TRUE)
-}
