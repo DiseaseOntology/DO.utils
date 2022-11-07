2892580afce386f10de47ee16855eaf4aff36525
J. Allen Baron <jabaron.phd@gmail.com>
2022-11-07 13:38:57 -0500

    Add tests for append_to_url() [PASS]

--- tests/testthat/test-append.R
+++ tests/testthat/test-append.R
@@ -30,3 +30,59 @@ test_that("append_empty_col() order argument works", {
         dplyr::select(dplyr::mutate(.input_df, z = NA), x, z, y)
     )
 })
+
+
+
+# append_to_url() ---------------------------------------------------------
+
+test_that("append_to_url() works", {
+    x <- "blah"
+    expect_equal(
+        append_to_url(x, "doi"),
+        "https://www.doi.org/blah"
+    )
+    expect_equal(
+        append_to_url(x, "github"),
+        "https://github.com/blah"
+    )
+    expect_equal(
+        append_to_url(x, "orcid"),
+        "https://orcid.org/blah"
+    )
+    expect_equal(
+        append_to_url(x, "pubmed"),
+        "https://pubmed.ncbi.nlm.nih.gov/blah"
+    )
+    expect_equal(
+        append_to_url(x, "pmc"),
+        "https://www.ncbi.nlm.nih.gov/pmc/blah"
+    )
+    expect_equal(
+        append_to_url(x, "pmc_article"),
+        "https://www.ncbi.nlm.nih.gov/pmc/articles/blah"
+    )
+    expect_equal(
+        append_to_url(x, "alliance_disease_tsv"),
+        "https://fms.alliancegenome.org/download/DISEASE-ALLIANCE_COMBINED.tsv.gz/blah"
+    )
+    expect_equal(
+        append_to_url(x, "http://fake.url.com"),
+        "http://fake.url.com/blah"
+    )
+    expect_equal(
+        append_to_url(x, "http://fake.url.com/"),
+        "http://fake.url.com/blah"
+    )
+})
+
+test_that("append_to_url() preserve_NA argument works", {
+    x <- c("blah", NA)
+    expect_equal(
+        append_to_url(x, "http://fake.url.com", preserve_NA = TRUE),
+        c("http://fake.url.com/blah", NA)
+    )
+    expect_equal(
+        append_to_url(x, "http://fake.url.com/", preserve_NA = FALSE),
+        c("http://fake.url.com/blah", "http://fake.url.com/NA")
+    )
+})
