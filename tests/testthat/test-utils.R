9dad3e3c8d8ff4dd2acf5b1750554e71ec7fa7b2
J. Allen Baron <jabaron.phd@gmail.com>
2022-12-22 15:30:59 -0500

    Add tests for all_duplicated() [PASS]
    Include test ignoring NA values (incomparables = NA).

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -15,3 +15,27 @@ test_that("sandwich_text() works", {
     )
     expect_error(sandwich_text("a", 1))
 })
+
+
+test_that("all_duplicated() works", {
+    expect_equal(all_duplicated(1:10), rep(FALSE, 10))
+    expect_equal(
+        all_duplicated(c(1, 1:8, 1)),
+        c(TRUE, TRUE, rep(FALSE, 7), TRUE)
+    )
+    expect_equal(
+        all_duplicated(c(NA, 1, 1:7, NA)),
+        c(rep(TRUE, 3), rep(FALSE, 6), TRUE)
+    )
+})
+
+test_that("all_duplicated(incomparables = NA) works", {
+    expect_equal(
+        all_duplicated(1:10, incomparables = NA),
+        rep(FALSE, 10)
+    )
+    expect_equal(
+        all_duplicated(c(NA, 1, 1:7, NA), incomparables = NA),
+        c(FALSE, rep(TRUE, 2), rep(FALSE, 6), FALSE)
+    )
+})
