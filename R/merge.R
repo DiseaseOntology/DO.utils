f1a7befb818591cd5a5de5653e4361388775550d
J. Allen Baron <jabaron.phd@gmail.com>
2022-12-22 17:28:54 -0500

    Create certify_unique_citations()
    To ensure no duplicate publication IDs exist prior to merging
    citations.

---
+++ R/merge.R
@@ -0,0 +1,63 @@
+# merge_citations() helpers -----------------------------------------------
+
+certify_unique_citations <- function(citation_df) {
+    df_nm <- as.character(substitute(citation_df))
+
+    id_present <- find_pub_id_cols(citation_df)
+    citation_id <- dplyr::select(citation_df, id_present)
+
+    row_na <- apply(citation_id, 1, function(x) { all(is.na(x)) })
+    if (any(row_na)) {
+        rlang::abort(
+            c(
+                paste0(
+                    "All rows in `", df_nm,
+                    "` must contain at least one identifier."
+                ),
+                x = paste0("Failing rows: ", to_range(which(row_na)))
+            ),
+            class = "citation_df-row_na",
+            .frame = parent.frame()
+        )
+    }
+
+    row_dup <- all_duplicated(citation_id)
+    if (any(row_dup)) {
+        rlang::abort(
+            c(
+                paste0("Rows in `", df_nm, "` must be unique."),
+                x = paste0("Failing rows: ", to_range(which(row_dup)))
+            ),
+            class = "citation_df-row_dup",
+            .frame = parent.frame()
+        )
+    }
+
+    dup_id_list <- purrr::map(
+        citation_id,
+        all_duplicated,
+        incomparables = NA
+    ) %>%
+        purrr::set_names(names(citation_id))
+
+    if (any(unlist(dup_id_list), na.rm = TRUE)) {
+        dup_id_rng <- purrr::map(dup_id_list, ~ to_range(which(.x))) %>%
+            unlist() %>%
+            na.omit()
+        rlang::abort(
+            c(
+                paste0(
+                    "All identifiers in `", df_nm, "` must be unique."
+                ),
+                purrr::set_names(
+                    paste0(names(dup_id_rng), ": ", dup_id_rng),
+                    nm = rep("x", length(dup_id_rng))
+                )
+            ),
+            class = "citation_df-id_dup",
+            .frame = parent.frame()
+        )
+    }
+
+    invisible(TRUE)
+}
---
+++ tests/testthat/test-merge.R
@@ -0,0 +1,45 @@
+test_that("certify_unique_citations() works", {
+    # no problems
+    citation_df <- tibble(
+        fake1 = 1:4,
+        fake2 = letters[1:4],
+        pmid = c("36157238", "36104825","36085310", "35967426"),
+        pmcid = c("PMC9499748", "PMC9472414", "PMC9463154", "PMC9365668"),
+        doi = c("10.1155/2022/2469592", "10.1186/s12957-022-02760-6",
+                "10.1038/s41467-022-33026-0", "10.3389/fimmu.2022.942446")
+    )
+
+    expect_equal(certify_unique_citations(citation_df), TRUE)
+
+
+    # 5th row: all IDs = NA
+    citation_rowna <- tibble::add_row(
+        citation_df,
+        fake1 = 5, fake2 = letters[5], doi = NA, pmid = NA, pmcid = NA
+    )
+    expect_error(
+        certify_unique_citations(citation_rowna),
+        class = "citation_df-row_na",
+        regexp = "5"
+    )
+
+
+    # copy 1st row as 5th
+    citation_row_dup <- dplyr::bind_rows(citation_df, citation_df[1, ])
+    expect_error(
+        certify_unique_citations(citation_row_dup),
+        class = "citation_df-row_dup",
+        regexp = "1,5"
+    )
+
+    # replace 4th pmid & doi with 1st, 2nd pmcid with 1st
+    citation_id_dup <- citation_df
+    citation_id_dup[4, c("pmid", "doi")] <- citation_id_dup[1, c("pmid", "doi")]
+    citation_id_dup[2, "pmcid"] <- citation_id_dup[1, "pmcid"]
+    expect_error(
+        certify_unique_citations(citation_id_dup),
+        class = "citation_df-id_dup",
+        # msg truncated = can't include confirmation for doi
+        regexp = c("pmid: 1,4.*pmcid: 1,2")
+    )
+})
