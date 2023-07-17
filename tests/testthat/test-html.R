45587e198e5ca198849e3fc25f3e8598e279cba9
J. Allen Baron <jabaron.phd@gmail.com>
2023-07-17 10:30:39 -0400

    Create build_hyperlink() tests for CURIE conversion
    Tests: FAIL (new only)

--- tests/testthat/test-html.R
+++ tests/testthat/test-html.R
@@ -38,3 +38,37 @@ test_that("build_hyperlink() works with vectors", {
         gs_expect
     )
 })
+
+test_that("build_hyperlink() works for CURIEs with known prefixes that have URLs available", {
+    curie <- c("DOID:4", "ICD9CM:758.0", "ICD10CM:Q90",
+               "KEGG:05034", "MESH:D004314", "NCI:C2993", "OMIM:190685",
+               "ORDO:870", "SNOMEDCT_US:41040004", "UMLS_CUI:C0013080")
+    expect <- c(
+        '<a href="http://purl.obolibrary.org/obo/DOID_4">DOID:4</a>',
+        '<a href="http://icd9cm.chrisendres.com/index.php?action=search&srchtext=758.0">ICD9CM:758.0</a>',
+        '<a href="http://www.icd10data.com/Search.aspx?search=Q90">ICD10CM:Q90</a>',
+        '<a href="https://www.kegg.jp/pathway/hsa05034">KEGG:05034</a>',
+        '<a href="https://meshb.nlm.nih.gov/record/ui?ui=D004314">MESH:D004314</a>',
+        '<a href="https://ncit.nci.nih.gov/ncitbrowser/ConceptReport.jsp?dictionary=NCI_Thesaurus&ns=ncit&code=C2993">NCI:C2993</a>',
+        '<a href="http://www.omim.org/MIM:190685">OMIM:190685</a>',
+        '<a href="https://www.orpha.net/consor/cgi-bin/OC_Exp.php?lng=en&Expert=870">ORDO:870</a>',
+        '<a href="https://browser.ihtsdotools.org/?perspective=full&conceptId1=41040004">SNOMEDCT_US:41040004</a>',
+        '<a href="https://uts.nlm.nih.gov/uts/umls/concept/C0013080">UMLS_CUI:C0013080</a>'
+    )
+    expect_equal(
+        build_hyperlink(
+            x = curie,
+            url = "from_curie",
+            as = "html"
+        ),
+        expect
+    )
+})
+
+test_that("build_hyperlink() preserves CURIEs with known prefixes without URLs available", {
+    curie <- "GARD:10247"
+    expect_equal(
+        build_hyperlink(x = curie, url = "from_curie", as = "html"),
+        curie
+    )
+})
