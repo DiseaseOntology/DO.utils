a2fb9256feb9eaf214d9c5370b0bb26bdedb40b5
J. Allen Baron <jabaron.phd@gmail.com>
2023-10-06 14:37:18 -0400

    Create round_down()
    Tests: PASS

--- R/math.R
+++ R/math.R
@@ -18,3 +18,25 @@ round_up <- function(x, digits = 0) {
     place <- 10^(-digits)
     x + place - x %% place
 }
+
+
+#' Round a number down
+#'
+#' @details
+#' Rounding to a negative number of digits means rounding to a power of ten, so
+#' for example round_down(x, digits = -2) rounds to the nearest hundred.
+#'
+#' @param x a numeric vector.
+#' @param digits integer indicating the number of decimal places. Negative
+#' values are allowed (see 'Details').
+#'
+#' @export
+round_down <- function(x, digits = 0) {
+
+    if (digits == 0) {
+        return(floor(x))
+    }
+
+    place <- 10^(-digits)
+    x - x %% place
+}
--- tests/testthat/test-math.R
+++ tests/testthat/test-math.R
@@ -6,3 +6,12 @@ test_that("round_up() works", {
     expect_equal(round_up(1.356, -2), 100)
     expect_equal(round_up(-1.356, -2), 0)
 })
+
+test_that("round_down() works", {
+    expect_equal(round_down(1.356, 2), 1.35)
+    expect_equal(round_down(-1.356, 2), -1.36)
+    expect_equal(round_down(1.356, 0), 1)
+    expect_equal(round_down(-1.356, 0), -2)
+    expect_equal(round_down(1.356, -2), 0)
+    expect_equal(round_down(-1.356, -2), -100)
+})
