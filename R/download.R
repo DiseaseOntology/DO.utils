e471de5fb66b57b2f322d0de37f5b55700c745a5
J. Allen Baron <jabaron.phd@gmail.com>
2024-02-09 10:21:30 -0500

    Reorder functions in download.R

--- R/download.R
+++ R/download.R
@@ -1,109 +1,3 @@
-#' Download File(s) from the Internet
-#'
-#' `download_file()` is a vectorized version of [utils::download.file()] that
-#' does not rely on specifying the "libcurl" method for vectorization.
-#'
-#' @param url A character vector naming the URL of resource(s) to be downloaded.
-#' @param dest_file A character vector with the file path(s) where downloaded
-#'     file(s) will be saved.
-#' @param on_failure A string indicating how to handle download failure:
-#' - "warn" - produce a warning; includes exit codes for debugging
-#' - "abort" - abort execution
-#' - "list_failed" - list URLs that failed (_output format differs_, see
-#'     `Value`)
-#' - "warn-list_failed" - combination of "warn" and "list_failed"
-#' - "skip" - do nothing
-#' @param ... Additional arguments passed on to [utils::download.file()].
-#'
-#' @return
-#' Unless `on_failure` includes "list_failed", the successfully downloaded
-#' `dest_file`(s); otherwise, a 2-vector list where `successful` =
-#' `dest_file`(s) and `failed` = `url`(s).
-#'
-#' @export
-download_file <- function(url, dest_file, on_failure = "warn", ...) {
-    assertthat::assert_that(length(dest_file) == length(url))
-    on_failure <- match.arg(
-        on_failure,
-        choices = c("warn", "abort", "list_failed", "warn-list_failed", "skip")
-    )
-
-    dl_status <- download_status$new()
-    purrr::map2(
-        .x = url,
-        .y = dest_file,
-        .f = function(.url, .file) {
-            utils::download.file(url = .url, destfile = .file, ...) %>%
-                dl_status$check(.url, .file, abort = on_failure == "abort")
-        }
-    )
-
-    if (stringr::str_detect(on_failure, "^warn")) {
-        dl_status$warn()
-    }
-
-    dl_status$return(
-        w_failed = stringr::str_detect(on_failure, "list_failed$")
-    )
-}
-
-
-#' Download OBO Foundry Ontology File
-#'
-#' Downloads the current version of one or more ontologies from the OBO Foundry.
-#'
-#' @param ontology_id A character vector of OBO Foundry ontology identifier(s)
-#'     (lowercase, as found on http://www.obofoundry.org/). For reference,
-#'     ontology identifiers are also provided in [obofoundry_metadata] within
-#'     this package.
-#' @param dest_dir Path to directory where files will be saved.
-#' @inheritParams download_file
-#'
-#' @inherit download_file return
-#'
-#' @export
-download_obo_ontology <- function(ontology_id, dest_dir, on_failure = "warn",
-                                  ...) {
-
-    # validate ontology_id
-    oid <- match.arg(
-        ontology_id,
-        choices = obofoundry_metadata$id,
-        several.ok = TRUE
-    )
-    assertthat::assert_that(
-        length(oid) == length(ontology_id),
-        msg = paste0(
-            "ontology_id(s) do not match OBO Foundry ontology ID(s): ",
-            vctr_to_string(ontology_id[!ontology_id %in% oid], delim = ", ")
-        )
-    )
-
-    # subset to non-obsolete ontologies and set dest_file
-    obofoundry_records <- obofoundry_metadata %>%
-        dplyr::filter(.data$id %in% oid & !.data$is_obsolete) %>%
-        dplyr::mutate(
-            dest_file = file.path(dest_dir, basename(.data$ontology_purl))
-        )
-
-    # warn about obsolete ontologies, which are not available for download
-    obsolete <- oid[!oid %in% obofoundry_records$id]
-    if (length(obsolete) > 0) {
-        rlang::warn(
-            message = c("Obsolete ontologies will be skipped", obsolete)
-        )
-    }
-
-    # download ontologies
-    download_file(
-        url = obofoundry_records$ontology_purl,
-        dest_file = obofoundry_records$dest_file,
-        on_failure = on_failure,
-        ...
-    )
-}
-
-
 #' Download Alliance .tsv.gz File
 #'
 #' Downloads a URL-specified .tsv.gz file from the Alliance of Genome
@@ -171,9 +65,119 @@ download_alliance_tsv <- function(dest_dir, url = NULL, ...) {
     download_file(url, dest_file, on_failure = "abort", ...)
 }
 
+
+#' Download OBO Foundry Ontology File
+#'
+#' Downloads the current version of one or more ontologies from the OBO Foundry.
+#'
+#' @param ontology_id A character vector of OBO Foundry ontology identifier(s)
+#'     (lowercase, as found on http://www.obofoundry.org/). For reference,
+#'     ontology identifiers are also provided in [obofoundry_metadata] within
+#'     this package.
+#' @param dest_dir Path to directory where files will be saved.
+#' @inheritParams download_file
+#'
+#' @inherit download_file return
+#'
+#' @export
+download_obo_ontology <- function(ontology_id, dest_dir, on_failure = "warn",
+                                  ...) {
+
+    # validate ontology_id
+    oid <- match.arg(
+        ontology_id,
+        choices = obofoundry_metadata$id,
+        several.ok = TRUE
+    )
+    assertthat::assert_that(
+        length(oid) == length(ontology_id),
+        msg = paste0(
+            "ontology_id(s) do not match OBO Foundry ontology ID(s): ",
+            vctr_to_string(ontology_id[!ontology_id %in% oid], delim = ", ")
+        )
+    )
+
+    # subset to non-obsolete ontologies and set dest_file
+    obofoundry_records <- obofoundry_metadata %>%
+        dplyr::filter(.data$id %in% oid & !.data$is_obsolete) %>%
+        dplyr::mutate(
+            dest_file = file.path(dest_dir, basename(.data$ontology_purl))
+        )
+
+    # warn about obsolete ontologies, which are not available for download
+    obsolete <- oid[!oid %in% obofoundry_records$id]
+    if (length(obsolete) > 0) {
+        rlang::warn(
+            message = c("Obsolete ontologies will be skipped", obsolete)
+        )
+    }
+
+    # download ontologies
+    download_file(
+        url = obofoundry_records$ontology_purl,
+        dest_file = obofoundry_records$dest_file,
+        on_failure = on_failure,
+        ...
+    )
+}
+
+
+# General Download --------------------------------------------------------
+
+#' Download File(s) from the Internet
+#'
+#' `download_file()` is a vectorized version of [utils::download.file()] that
+#' does not rely on specifying the "libcurl" method for vectorization.
+#'
+#' @param url A character vector naming the URL of resource(s) to be downloaded.
+#' @param dest_file A character vector with the file path(s) where downloaded
+#'     file(s) will be saved.
+#' @param on_failure A string indicating how to handle download failure:
+#' - "warn" - produce a warning; includes exit codes for debugging
+#' - "abort" - abort execution
+#' - "list_failed" - list URLs that failed (_output format differs_, see
+#'     `Value`)
+#' - "warn-list_failed" - combination of "warn" and "list_failed"
+#' - "skip" - do nothing
+#' @param ... Additional arguments passed on to [utils::download.file()].
+#'
+#' @return
+#' Unless `on_failure` includes "list_failed", the successfully downloaded
+#' `dest_file`(s); otherwise, a 2-vector list where `successful` =
+#' `dest_file`(s) and `failed` = `url`(s).
+#'
+#' @export
+download_file <- function(url, dest_file, on_failure = "warn", ...) {
+    assertthat::assert_that(length(dest_file) == length(url))
+    on_failure <- match.arg(
+        on_failure,
+        choices = c("warn", "abort", "list_failed", "warn-list_failed", "skip")
+    )
+
+    dl_status <- download_status$new()
+    purrr::map2(
+        .x = url,
+        .y = dest_file,
+        .f = function(.url, .file) {
+            utils::download.file(url = .url, destfile = .file, ...) %>%
+                dl_status$check(.url, .file, abort = on_failure == "abort")
+        }
+    )
+
+    if (stringr::str_detect(on_failure, "^warn")) {
+        dl_status$warn()
+    }
+
+    dl_status$return(
+        w_failed = stringr::str_detect(on_failure, "list_failed$")
+    )
+}
+
+
 # Import 'new()' method for Ref Classes
 #' @importFrom methods new
 NULL
+
 
 #' File Download Status (RefClass)
 #'
