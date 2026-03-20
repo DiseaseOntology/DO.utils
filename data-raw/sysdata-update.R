014006c18c76e2ab036bad7c77b6ee7597c95f13
J. Allen Baron <jabaron.phd@gmail.com>
2026-03-20 17:40:21 -0400

    Completely refactor internal data management
    Modification of #34 for full curation template work

---
+++ data-raw/sysdata-update.R
@@ -0,0 +1,34 @@
+rlang::check_installed("here", "usethis")
+
+indir <- here::here("data-raw", "internal")
+
+
+# DO Google Sheets reference
+.DO_gs <- readRDS(file.path(indir, "DO_gs.rds"))
+
+
+# HTML tags reference
+.html_tags <- readRDS(file.path(indir, "html_tags.rds"))
+
+
+# curation template specification
+.curation_opts <- readRDS(file.path(indir, "curation_opts.rds"))
+.sparql_dt_motif <- readRDS(file.path(indir, "sparql_dt_motif.rds"))
+
+# SSSOM specification
+.sssom_spec <- readRDS(file.path(indir, "sssom_spec.rds"))
+.sssom_slot_types <- readRDS(file.path(indir, "sssom_slot_types.rds"))
+.sssom_mapping_slots <- readRDS(file.path(indir, "sssom_mapping_slots.rds"))
+
+
+usethis::use_data(
+  .DO_gs,
+  .html_tags,
+  .curation_opts,
+  .sparql_dt_motif,
+  .sssom_spec,
+  .sssom_slot_types,
+  .sssom_mapping_slots,
+  internal = TRUE,
+  overwrite = TRUE
+)
