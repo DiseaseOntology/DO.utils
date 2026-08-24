f7618ee9fb024df28dcfbc444d924a7f4b7ccd45
J. Allen Baron <jabaron.phd@gmail.com>
2026-08-24 17:46:17 -0400

    Export get_html_indent()

--- R/html.R
+++ R/html.R
@@ -299,11 +299,25 @@ get_html_table <- function(html, id) {
   html_tbl
 }
 
+#' Get HTML Indentation Data
+#'
+#' @description
+#' Get indentation data from HTML code, including the preferred indent type
+#' (space or tab), the base indent (the minimum indent across all lines), and
+#' the indent increment (the minimum increment above the base indent across all
+#' lines).
+#'
+#' This function is useful for formatting HTML code to improve readability and
+#' maintain consistent indentation.
+#'
+#' @param html HTML code as a character vector or a string with newline
+#'   characters.
 #' @returns A list of the form `list(type = " ", base = "  ", unit = "  ")`,
 #' where `type` is the preferred indent type (space or tab), `base` is the
 #' base indent (the minimum indent across all lines), and `unit` is the indent
 #' increment (the minimum increment above the base indent across all lines).
-#' @noRd
+#'
+#' @export
 get_html_indent <- function(html) {
   if (length(html) == 1) {
     html <- stringr::str_split(html, "\n")[[1]]
